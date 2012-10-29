2e85d622e89f8ba859e44c07ad5372f8f5f09517
Andrew Brown & Charles Hansen <pair+abrown+chansen@pivotallabs.com>
2012-10-29 10:11:55 -0700

    [#36982615] update integration script for running backup and restore

--- script/ci/backup_and_restore.sh
+++ script/ci/backup_and_restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+hostname = $1
 echo "Running backup"
-chorus_control.sh backup -d backups
+ssh $hostname chorus_control.sh backup -d backups
 echo "Running restore"
-chorus_control.sh restore backups/*.tar
+ssh $hostname chorus_control.sh restore backups/*.tar
