f4dc7e96b5501f5d9183293861e0c49f85f8badf
Joseph Palermo <pair+joseph@pivotallabs.com>
2012-10-30 13:54:05 -0700

    remove postgres 9.1 from path in ci

--- script/ci/setup.sh
+++ script/ci/setup.sh
@@ -1,5 +1,5 @@
 export JRUBY_OPTS="--1.9 --client -J-Xmx512m -J-Xms512m -J-Xmn128m"
-export PATH="$HOME/phantomjs/bin:/opt/postgres/9.1/bin:$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/phantomjs/bin:$HOME/.rbenv/bin:$PATH"
 
 eval "$(rbenv init - --no-rehash)"
 rbenv shell `cat .rbenv-version`
