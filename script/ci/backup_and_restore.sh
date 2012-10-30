e8111e8a024119f228f904460f359e314ef18333
Andrew Brown & Helge Holzmann <pair+abrown+helge@pivotallabs.com>
2012-10-30 10:04:23 -0700

    change path in backup_and_restore script to use an absolute path

--- script/ci/backup_and_restore.sh
+++ script/ci/backup_and_restore.sh
@@ -2,7 +2,7 @@
 hostname=$1
 dir=$2
 echo "Running backup"
-ssh $hostname "cd $dir && CHORUS_HOME=$dir ./chorus_control.sh backup -d backups"
+ssh $hostname "cd $dir && CHORUS_HOME=$dir ./chorus_control.sh backup -d $dir/backups"
 echo "Running restore"
-ssh $hostname "cd $dir && CHORUS_HOME=$dir ./chorus_control.sh restore -s backups/*.tar"
+ssh $hostname "cd $dir && CHORUS_HOME=$dir ./chorus_control.sh restore -s $dir/backups/*.tar"
 
