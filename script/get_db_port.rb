44854b6c8114be07545c76530d8990eb007f97b2
Bleicke Petersen, Fiona Tay & Helge Holzmann <pair+bpetersen+fiona+helge@pivotallabs.com>
2012-11-01 15:11:17 -0700

    I know we jsut met and this is crazy but we just fixed the build maybe

--- script/get_db_port.rb
+++ script/get_db_port.rb
@@ -1,5 +1,5 @@
 require_relative("../lib/properties")
 
-config = Properties.load_file("config/chorus.properties")
+config = Properties.load_file("#{File.dirname(__FILE__)}/../config/chorus.properties")
 puts config['postgres_port']
 
