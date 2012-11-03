a6b6de5fbb3cf16f608879d36f890357569aae8b
Andrew Brown & Chris Brown <pair+abrown+cbrown@pivotallabs.com>
2012-11-02 17:51:33 -0700

    [#38886215] only expect connection to disconnect when given a block

--- spec/services/gpdb/connection_builder_spec.rb
+++ spec/services/gpdb/connection_builder_spec.rb
@@ -54,7 +54,8 @@ describe Gpdb::ConnectionBuilder do
 
       it "disconnects afterward" do
         mock(fake_connection_adapter).disconnect!
-        Gpdb::ConnectionBuilder.connect!(gpdb_instance, instance_account)
+        Gpdb::ConnectionBuilder.connect!(gpdb_instance, instance_account) do
+        end
       end
 
       context "when not given a block" do
