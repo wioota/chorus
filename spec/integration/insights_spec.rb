975a4b5d0b35e7616346df488b52e4a6ac835157
Callum Stott & Chris Brown <pair+callum+cbrown@pivotallabs.com>
2012-11-05 16:17:34 -0800

    [#38981217] Fix a pending insights integration spec.

--- spec/integration/insights_spec.rb
+++ spec/integration/insights_spec.rb
@@ -1,24 +1,23 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Insights" do
-
-  xit "clicks on the insights link on the home page" do
-    login('edcadmin', 'secret')
+   it "clicks on the insights link on the home page" do
+    login(users(:owner))
     wait_for_ajax
     click_link "Insights"
     wait_for_ajax
     wait_until {page.find(".title h1").text == "Insights"}
   end
 
-  xit "creates an insight" do
-    login('edcadmin', 'secret')
-    create_valid_workspace
+  it "creates an insight" do
+    login(users(:owner))
+    click_link workspaces(:public).name
     wait_until { page.find('a[data-dialog="InsightsNew"]').text == "Add an insight"}
     click_link "Add an insight"
 
     within_modal do
       set_cleditor_value("body", "This is adding an Insight")
-      click_submit_button
+      click_on "Add Insight"
     end
   end
 end
