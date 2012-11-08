520e70c1e52f9fbf557202b3ea7710704ccff2a9
Bleicke Petersen & Charles Hansen <pair+bpetersen+chansen@pivotallabs.com>
2012-11-08 13:46:28 -0800

    Use absolute path in setting legacy path [#38695883]

--- packaging/install/chorus_installer.rb
+++ packaging/install/chorus_installer.rb
@@ -287,7 +287,7 @@ class ChorusInstaller
 
   def dump_and_shutdown_legacy
     Dir.chdir legacy_installation_path do
-      set_env = "source edc_path.sh"
+      set_env = "source #{legacy_installation_path}/edc_path.sh"
       log "Shutting down Chorus..." do
         chorus_exec("#{set_env} && bin/edcsvrctl stop; true")
       end
