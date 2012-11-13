9342bff2457dcc78c24ff69acaed6a4d796ec820
Brandon Liu and Jesse Smith <pair+brandon+jsmith@pivotallabs.com>
2012-11-12 16:53:54 -0800

    update jetty xml script to use rexml instead of nokogiri

--- packaging/update_jetty_xml.rb
+++ packaging/update_jetty_xml.rb
@@ -1,20 +1,20 @@
 chorus_home = File.expand_path(File.dirname(__FILE__) + '/../')
-require 'nokogiri'
+require 'rexml/document'
 require File.join(chorus_home, 'app/models/chorus_config')
 
 jetty_xml_file = File.join(chorus_home, 'vendor', 'jetty', 'jetty.xml')
 
 chorus_config = ChorusConfig.new(chorus_home)
 
-xml_doc = Nokogiri::XML(File.new(jetty_xml_file))
-max_threads = xml_doc.xpath("/Configure/Set[@name='ThreadPool']/New/Set[@name='maxThreads']").first
-max_threads.content = chorus_config['webserver_threads']
+xml_doc = REXML::Document.new(File.new(jetty_xml_file))
+max_threads = REXML::XPath.first(xml_doc, "/Configure/Set[@name='ThreadPool']/New/Set[@name='maxThreads']")
+max_threads.text = chorus_config['webserver_threads']
 
-min_threads = xml_doc.xpath("/Configure/Set[@name='ThreadPool']/New/Set[@name='minThreads']").first
-if min_threads.content.to_i > chorus_config['webserver_threads'].to_i
-  min_threads.content = chorus_config['webserver_threads']
+min_threads = REXML::XPath.first(xml_doc, "/Configure/Set[@name='ThreadPool']/New/Set[@name='minThreads']")
+if min_threads.text.to_i > chorus_config['webserver_threads'].to_i
+  min_threads.text = chorus_config['webserver_threads']
 end
 
 File.open(jetty_xml_file, 'w') do |f|
-  f.write(xml_doc.to_xml)
+  f.write(xml_doc.to_s)
 end
