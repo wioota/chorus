3bb211486a356af0ee87a605ed7c728f7b6c78f1
Andrew Brown & Helge Holzmann <pair+abrown+helge@pivotallabs.com>
2012-11-14 13:46:37 -0800

    [fix #39516529] swap dataset and source_dataset between target1 and target2 for DatasetImportCreated in order to match other events

--- app/models/events/dataset_import_failed.rb
+++ app/models/events/dataset_import_failed.rb
@@ -2,7 +2,7 @@ require 'events/base'
 
 module Events
   class DatasetImportFailed < Base
-    has_targets :dataset, :source_dataset, :workspace
+    has_targets :source_dataset, :dataset, :workspace
     has_activities :actor, :workspace, :source_dataset, :dataset
     has_additional_data :destination_table, :error_message
   end
