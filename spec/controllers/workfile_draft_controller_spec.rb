95391cd7a8a2c3bd79d6e078fa988bca7c478331
Chris Brown & Corina Lawu <pair+cbrown+corina@pivotallabs.com>
2012-11-14 09:19:07 -0800

    [#37273151] Add authorization to workfile drafts.

--- spec/controllers/workfile_draft_controller_spec.rb
+++ spec/controllers/workfile_draft_controller_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe WorkfileDraftController do
-  let(:valid_attributes) { { :content => "Valid content goes here", :workfile_id => 3939 } }
+  let(:workspace) { workspaces(:private) }
   let(:user) { users(:owner) }
+  let(:workfile) { FactoryGirl.create(:workfile, :file_name => "workfile.txt",
+                                      :owner => user, :workspace => workspace) }
+  let(:valid_attributes) { { :content => "Valid content goes here", :workfile_id => workfile.id } }
   let!(:draft) do
     workfile_drafts(:default).tap do |draft|
       draft.content = "Valid content goes here"
       draft.owner_id = user.id
-      draft.workfile_id = 3939
+      draft.workfile_id = workfile.id
     end
   end
 
@@ -25,8 +28,13 @@ describe WorkfileDraftController do
       it "renders the newly created draft" do
         post :create, valid_attributes
         decoded_response.content.should == "Valid content goes here"
-        decoded_response.workfile_id.should == 3939
+        decoded_response.workfile_id.should == workfile.id
         decoded_response.owner_id.should == user.id
+      end
+
+      it "uses authorization" do
+        mock(subject).authorize! :can_edit_sub_objects, workfile.workspace
+        post :create, valid_attributes
       end
     end
   end
@@ -36,15 +44,20 @@ describe WorkfileDraftController do
       draft.save!
     end
     it "renders the specified draft" do
-      get :show, :workfile_id => 3939
+      get :show, :workfile_id => workfile.id
       decoded_response.content.should == "Valid content goes here"
-      decoded_response.workfile_id.should == 3939
+      decoded_response.workfile_id.should == workfile.id
       decoded_response.owner_id.should == user.id
       decoded_response.id.should == draft.id
     end
 
+    it "uses authorization" do
+      mock(subject).authorize! :show, workfile.workspace
+      get :show, :workfile_id => workfile.id
+    end
+
     generate_fixture "draft.json" do
-      get :show, :workfile_id => 3939
+      get :show, :workfile_id => workfile.id
     end
   end
 
@@ -53,24 +66,34 @@ describe WorkfileDraftController do
       draft.save!
     end
     it "updates and renders the updated values" do
-      put :update, :content => "I am a leaf upon the wind, watch how I soar.", :workfile_id => 3939
+      put :update, :content => "I am a leaf upon the wind, watch how I soar.", :workfile_id => workfile.id
       decoded_response.content.should == "I am a leaf upon the wind, watch how I soar."
-      decoded_response.workfile_id.should == 3939
+      decoded_response.workfile_id.should == workfile.id
       decoded_response.owner_id.should == user.id
       decoded_response.id.should == draft.id
+    end
+
+    it "uses authorization" do
+      mock(subject).authorize! :can_edit_sub_objects, workfile.workspace
+      put :update, :content => "I am a leaf upon the wind, watch how I soar.", :workfile_id => workfile.id
     end
   end
 
   describe "#delete" do
     it "deletes the draft" do
       draft.save!
-      delete :destroy, :workfile_id => 3939
+      delete :destroy, :workfile_id => workfile.id
       response.should be_success
     end
 
     it "does not delete anything if given a non-existing draft" do
-      lambda { delete :destroy, :workfile_id => 3939 }.should_not change { WorkfileDraft.count }
+      lambda { delete :destroy, :workfile_id => workfile.id }.should_not change { WorkfileDraft.count }
       response.code.should == '404'
+    end
+
+    it "uses authorization" do
+      mock(subject).authorize! :can_edit_sub_objects, workfile.workspace
+      delete :destroy, :workfile_id => workfile.id
     end
   end
 end
