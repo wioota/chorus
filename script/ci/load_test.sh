8ddf72ed750c309bb8d81567d488db0b385c74b6
Charles Hansen & Gregg Van Hove <pair+chansen+gvanhove@pivotallabs.com>
2012-11-15 11:28:37 -0800

    [#38891969] Add build script for load test server

---
+++ script/ci/load_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+for file in greenplum-chorus-*.sh
+do
+    echo "Deploying $file"
+    chmod +x $file
+    rake deploy["load-test","$file"]
+    break
+done
+
+rake bulk_data:generate_fake_users["load-test-users.csv",200]
+ #setup_load_test["load-test","load-test-users.csv"]
