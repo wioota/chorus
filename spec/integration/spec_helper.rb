64f0cb26ab8538fb09f4e3d1a234b180ee237b32
Pivotal Labs <ops+github@pivotallabs.com>
2012-11-16 14:15:19 -0800

    fix screenshots

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../../config/environment", __FILE__)
 
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'capybara-screenshot'
+require 'capybara-screenshot/rspec'
 require 'headless'
 require 'yaml'
 
