aaf4ba6dd12ec1f41de85287223e1f924d316e3e
Callum Stott & Joseph Palermo <pair+callum+joseph@pivotallabs.com>
2012-11-19 11:04:24 -0800

    [#39009919] Tests for searchable_html

--- lib/searchable_html.rb
+++ lib/searchable_html.rb
@@ -1,30 +1,15 @@
 module SearchableHtml
-  def self.included(base)
-    base.extend ClassMethods
-  end
+  extend ActiveSupport::Concern
+  include ActionView::Helpers::SanitizeHelper
 
   module ClassMethods
     def searchable_html(field, options = {})
       searchable options do
         text field, :stored => true do
           html = send field
-          remove_tags html
+          strip_tags html
         end
       end
-    end
-
-    private
-
-    def remove_tags(html)
-      result = ""
-      doc = Nokogiri::HTML(html)
-      doc.xpath("//text()").each do |node|
-        if result.length > 0
-          result += " "
-        end
-        result += node.to_s
-      end
-      result
     end
   end
 end
--- spec/lib/searchable_html_spec.rb
+++ spec/lib/searchable_html_spec.rb
@@ -3,35 +3,31 @@ require 'sunspot'
 
 describe SearchableHtml do
   describe ".searchable_html" do
-    #TODO: Get this working, it's close
-    #
-    #class TestClass < ActiveRecord::Base
-    #  @columns = []
-    #  include SearchableHtml
-    #
-    #  attr_accessor :html_field
-    #  attr_accessible :html_field
-    #
-    #  searchable_html :html_field
-    #end
-    #
-    #before do
-    #  Sunspot.session = Sunspot.session.original_session
-    #end
-    #
-    #it "removes tags from the body" do
-    #  any_instance_of(RSolr::Connection) do |connection|
-    #    mock(connection).execute.with_any_args do |client, request_context|
-    #      throw :success, "success" if request_context[:data].match %r{this\s+is\s+text}
-    #    end
-    #  end
-    #
-    #  obj = TestClass.new(:html_field => 'this <b>is text</b>')
-    #
-    #  catch(:success) {
-    #    obj.solr_index
-    #    false
-    #  }.should == "success"
-    #end
+    class TestClass < ActiveRecord::Base
+      @columns = []
+      include SearchableHtml
+
+      attr_accessor :html_field, :id
+      attr_accessible :html_field
+
+      searchable_html :html_field
+    end
+
+    before do
+      Sunspot.session = Sunspot.session.original_session
+    end
+
+    it "removes tags from the body" do
+      VCR.use_cassette("searchable_html") do
+        Sunspot.session.remove_all
+        obj = TestClass.new(:html_field => 'this <b>is text</b>')
+        obj.id = 1
+        obj.solr_index
+        Sunspot.commit
+
+        results = TestClass.search { fulltext "text" }
+        results.hits[0].stored('html_field')[0].should == 'this is text'
+      end
+    end
   end
 end
