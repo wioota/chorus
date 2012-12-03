0ce1b20cf51896ff9e77e64e7a12343a55deba9c
Joseph Palermo <pair+joseph@pivotallabs.com>
2012-12-03 14:58:01 -0800

    [#38304465] Stopping postgres or webserver when they are already stopped should have exit status of 0

--- packaging/stop-postgres.sh
+++ packaging/stop-postgres.sh
@@ -8,11 +8,21 @@ bin=`cd "$bin"; pwd`
 
 . "$bin"/chorus-config.sh
 
-log_inline "stopping postgres "
-$CHORUS_HOME/postgres/bin/pg_ctl -D $POSTGRES_DATA_DIR -m fast stop > /dev/null
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -eq 0 ]; then
-    wait_for_stop $POSTGRES_PID_FILE
+if [ -f $POSTGRES_PID_FILE ]; then
+  if kill -0 `cat $POSTGRES_PID_FILE` > /dev/null 2>&1; then
+    log_inline "stopping postgres "
+    $CHORUS_HOME/postgres/bin/pg_ctl -D $POSTGRES_DATA_DIR -m fast stop > /dev/null
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -eq 0 ]; then
+        wait_for_stop $POSTGRES_PID_FILE
+    fi
+    exit $EXIT_CODE
+  else
+    log "no postgres to stop"
+    rm -f $POSTGRES_PID_FILE
+    exit 0
+  fi
+else
+  log "no postgres to stop"
+  exit 0
 fi
-exit $EXIT_CODE
--- packaging/stop-webserver.sh
+++ packaging/stop-webserver.sh
@@ -9,6 +9,7 @@ bin=`cd "$bin"; pwd`
 . "$bin"/chorus-config.sh
 
 if [ -f $NGINX_PID_FILE ]; then
+  if kill -0 `cat $NGINX_PID_FILE` > /dev/null 2>&1; then
     log_inline "stopping nginx "
     cd $CHORUS_HOME/vendor/nginx/nginx_dist/
     OPENSSL_CONF=$OPENSSL_CONF ./$NGINX -s stop
@@ -17,28 +18,51 @@ if [ -f $NGINX_PID_FILE ]; then
         wait_for_stop $NGINX_PID_FILE
     fi
     cd $CHORUS_HOME
+  else
+    log "no nginx to stop"
+    rm -f $NGINX_PID_FILE
+    STOP_SUCCESS=0
+  fi
 else
     log "no nginx to stop"
-    STOP_SUCCESS=1
+    STOP_SUCCESS=0
 fi
 
 case $RAILS_ENV in
     development )
-        log_inline "stopping mizuno "
-        bundle exec mizuno --stop --pidfile $MIZUNO_PID_FILE &>/dev/null
-        STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
-        if [ $STOP_SUCCESS -eq 0 ]; then
-            wait_for_stop $MIZUNO_PID_FILE
+        if [ -f $MIZUNO_PID_FILE ]; then
+          if kill -0 `cat $MIZUNO_PID_FILE` > /dev/null 2>&1; then
+            log_inline "stopping mizuno "
+            bundle exec mizuno --stop --pidfile $MIZUNO_PID_FILE &>/dev/null
+            STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
+            if [ $STOP_SUCCESS -eq 0 ]; then
+                wait_for_stop $MIZUNO_PID_FILE
+            fi
+          else
+            log "no mizuno to stop"
+            rm -f $MIZUNO_PID_FILE
+          fi
+        else
+          log "no mizuno to stop"
         fi
         ;;
     * )
-        log_inline "stopping jetty "
-        cd $CHORUS_HOME/vendor/jetty/
-        JETTY_PID=$JETTY_PID_FILE RAILS_ENV=$RAILS_ENV ./jetty-init stop
-        STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
-        cd $CHORUS_HOME
-        if [ $STOP_SUCCESS -eq 0 ]; then
-            wait_for_stop $JETTY_PID_FILE
+        if [ -f $JETTY_PID_FILE ]; then
+          if kill -0 `cat $JETTY_PID_FILE` > /dev/null 2>&1; then
+            log_inline "stopping jetty "
+            cd $CHORUS_HOME/vendor/jetty/
+            JETTY_PID=$JETTY_PID_FILE RAILS_ENV=$RAILS_ENV ./jetty-init stop
+            STOP_SUCCESS=`expr $STOP_SUCCESS + $?`
+            cd $CHORUS_HOME
+            if [ $STOP_SUCCESS -eq 0 ]; then
+                wait_for_stop $JETTY_PID_FILE
+            fi
+          else
+            log "no jetty to stop"
+            rm -f $JETTY_PID_FILE
+          fi
+        else
+          log "no jetty to stop"
         fi
         ;;
 esac
