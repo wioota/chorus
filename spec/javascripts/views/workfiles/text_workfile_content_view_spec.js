af1ab842b9822365b4954c501522a106b265353b
Charles Hansen & Nitin Grewal <pair+chansen+nitin@pivotallabs.com>
2012-12-07 10:25:05 -0800

    [#fix CI] revert prior -' marking the test pending'

--- spec/javascripts/views/workfiles/text_workfile_content_view_spec.js
+++ spec/javascripts/views/workfiles/text_workfile_content_view_spec.js
@@ -17,7 +17,7 @@ describe("chorus.views.TextWorkfileContentView", function() {
         stubDefer();
     })
 
-    xdescribe("hotkey options", function() {
+    describe("hotkey options", function() {
         beforeEach(function() {
             this.view = new chorus.views.TextWorkfileContent({model: this.textfile, hotkeys: {a: "whatever", b: "something_else"}});
             this.view.render();
