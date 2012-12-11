447c7f0917c39f32cb83b16a164660a7ef410d36
Chris Brown and Corina Lawu <pair+cbrown+corina@pivotallabs.com>
2012-12-11 11:32:09 -0800

    [#39968637] Change integration tests to reflect 'instances' -> 'data sources' name change.

--- spec/integration/instances_spec.rb
+++ spec/integration/instances_spec.rb
@@ -5,7 +5,7 @@ describe "Instances", :database_integration do
     login(users(:admin))
     visit("#/instances")
     wait_for_ajax
-    click_button "Add instance"
+    click_button "Add Data Source"
   end
 
   describe "add a gpdb instance" do
@@ -17,7 +17,7 @@ describe "Instances", :database_integration do
         fill_in 'port', :with => WEBPATH['gpdb_instance_db']['gpdb_port']
         fill_in 'dbUsername', :with => WEBPATH['gpdb_instance_db']['gpdb_user']
         fill_in 'dbPassword', :with => WEBPATH['gpdb_instance_db']['gpdb_pass']
-        click_button "Add Instance"
+        click_button "Add Data Source"
       end
 
       find(".gpdb_instance ul").should have_content("new_gpdb_instance")
@@ -33,7 +33,7 @@ describe "Instances", :database_integration do
         fill_in 'port', :with => WEBPATH['hadoop_instance_db']['port']
         fill_in 'username', :with => WEBPATH['hadoop_instance_db']['username']
         fill_in 'groupList', :with => WEBPATH['hadoop_instance_db']['group_list']
-        click_button "Add Instance"
+        click_button "Add Data Source"
       end
 
       find(".hadoop_instance ul").should have_content("new_hadoop_instance")
