8ec56876f91cdd0e3532f98e73b6586a1a0f92ee
Andrew Brown & Corina Lawu <pair+abrown+corina@pivotallabs.com>
2012-12-13 16:52:26 -0800

    [#39920761] make tags case insensitive

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -2,7 +2,8 @@ class TaggingsController < ApplicationController
   def create
     model = Workfile.find(params[:entity_id])
     authorize! :update, model
-    model.tag_list = params[:tag_names].join ","
+    tag_names = params[:tag_names] || []
+    model.tag_list = tag_names.uniq(&:downcase).join ","
     model.save!
     render :json => {}, :status => :created
   end
--- spec/controllers/taggings_controller_spec.rb
+++ spec/controllers/taggings_controller_spec.rb
@@ -9,12 +9,23 @@ describe TaggingsController do
 
   describe 'create' do
     let(:workfile) { workfiles(:'sql.sql') }
-    let(:params) { { :entity_id => workfile.id, :entity_type => 'workfile', :tag_names => ['alpha', 'beta'] } }
+    let(:params) { { :entity_id => workfile.id, :entity_type => 'workfile', :tag_names => tag_names } }
+    let(:tag_names) { ['alpha', 'beta'] }
 
     it 'sets the tags to the workfile' do
       post :create, params
       response.code.should == '201'
       workfile.reload.tags.map(&:name).should =~ ['alpha', 'beta']
+    end
+
+    describe "when no tag names are provided" do
+      let(:tag_names) { [] }
+
+      it 'clears the list of tags on the model' do
+        post :create, params
+        response.code.should == '201'
+        workfile.reload.tags.should == []
+      end
     end
 
     context 'when the user does not have permission to tag the workfile' do
@@ -23,6 +34,16 @@ describe TaggingsController do
       it 'returns unauthorized' do
         post :create, params
         response.code.should == '403'
+      end
+    end
+
+    describe 'when tags differ only in case' do
+      let(:params) { { :entity_id => workfile.id, :entity_type => 'workfile', :tag_names => ['AlphaNotInFixtures', 'alphaNotInFixtures'] } }
+
+      it "sets a single tag on the workfile using the first tag's case" do
+        post :create, params
+        response.code.should == '201'
+        workfile.reload.tags.map(&:name).should == ['AlphaNotInFixtures']
       end
     end
   end
