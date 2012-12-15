dc40e938942188e67f7787556002f7dc347417aa
Chris Brown <pair+cbrown@pivotallabs.com>
2012-12-14 17:44:08 -0800

    [#39727051] Move to new method of checking if tables exist in CSV file class

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -30,25 +30,11 @@ class CsvFile < ActiveRecord::Base
 
   def table_already_exists(table_name)
     schema = workspace.sandbox
-    account = schema.gpdb_instance.account_for_user!(user)
-    check_table(table_name, account, schema)
+    schema.connect_as(user).table_exists?(table_name)
   end
 
   def ready_to_import?
     to_table.present? && column_names.present? && types.present? && file_contains_header != nil &&
     delimiter != nil && delimiter.length > 0 && valid?
-  end
-
-  private
-
-  def check_table(table_name, account, schema)
-    schema.with_gpdb_connection(account) do |connection|
-      check_results = connection.exec_query <<-SQL
-        SELECT table_name
-        FROM information_schema.tables
-        WHERE table_name = '#{table_name}' AND table_schema = '#{schema.name}'
-      SQL
-      check_results.count > 0
-    end
   end
 end
