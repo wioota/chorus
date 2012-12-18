c46305ee26d0779b186062bc9da2c510bfd0a743
Gregg Van Hove & Helge Holzmann <pair+gvanhove+helge@pivotallabs.com>
2012-12-18 14:25:50 -0800

    [#41045805] don't call methods you've deleted

--- packaging/install/chorus_installer.rb
+++ packaging/install/chorus_installer.rb
@@ -404,7 +404,6 @@ class ChorusInstaller
     end
 
     log "#{upgrade_existing? ? "Updating" : "Creating"} database..." do
-      link_services
       generate_paths_file
       generate_chorus_psql_files
       setup_database
