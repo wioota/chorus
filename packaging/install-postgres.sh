d396af4bb110133b2c23572148ef6642e88101c2
pivotal-leopold <greenplum-chorus@googlegroups.com>
2012-12-18 04:17:49 -0800

    Update packaging/install-postgres.sh
    Fix postgres installer

--- packaging/install-postgres.sh
+++ packaging/install-postgres.sh
@@ -6,17 +6,24 @@ wget http://ftp.postgresql.org/pub/source/v$POSTGRES_VERSION/postgresql-$POSTGRE
 tar xzf postgresql-$POSTGRES_VERSION.tar.gz
 cd postgresql-$POSTGRES_VERSION
 
+rm -rf /tmp/postgres
+
 # We need this setup so library path is searched at runtime based on the bin path (relocatable installation)
-./configure LDFLAGS='-Wl,-rpath,'\''$$ORIGIN'\''/../lib'  --disable-rpath --prefix=$HOME/postgres --without-zlib --without-readline
+./configure LDFLAGS='-Wl,-rpath,'\''$$ORIGIN'\''/../lib'  --disable-rpath --prefix=/tmp/postgres --without-zlib --without-readline
 
 make && make install
-cd ..
 
-echo "Packaging $postgres-$POSTGRES_VERSION.tar.gz.."
+pushd /tmp
+
+echo "Packaging postgres-$POSTGRES_VERSION.tar.gz.."
 tar czfh postgres-$POSTGRES_VERSION.tar.gz postgres/
 
 echo "Moving package to $CHORUS_HOME/packaging/postgres/.."
-mv postgres-$POSTGRES_VERSION.tar.gz packaging/postgres/
+
+popd
+
+cd ..
+mv /tmp/postgres-$POSTGRES_VERSION.tar.gz postgres/
 
 echo "Removing temp files.."
 rm postgresql-$POSTGRES_VERSION.tar.gz
