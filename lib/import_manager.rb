2bf1028f25ad93fb30e51443aba3a6a78e4bad10
Andrew Brown & Fiona Tay <pair+abrown+fiona@pivotallabs.com>
2012-12-20 15:18:56 -0800

    use rspec matchers [#41244423]

--- lib/import_manager.rb
+++ lib/import_manager.rb
@@ -8,7 +8,7 @@ class ImportManager < DelegateClass(Import)
     matcher = "%pipe%_#{created_at.to_i}_#{id}" + (type == :writer ? "_w" : "_r")
     result = db.connect_as(user).fetch("select procpid, current_query from pg_stat_activity where current_query LIKE '#{matcher}%' AND current_query NOT LIKE '%procpid%'")
     if result.count > 1
-      raise "Unxpected multiple procpids: #{result.inspect}"
+      raise "Unexpected multiple procpids: #{result.inspect}"
     end
     (result.last || {})[:procpid]
   end
