3e8fd0670addd639e7fdf8929dc473ad24f67f69
Andrew Brown & Chris Brown <pair+abrown+cbrown@pivotallabs.com>
2012-12-21 12:11:43 -0800

    record started_at for imports and handle multiple procpids for writer [#41244423]

--- spec/lib/import_manager_spec.rb
+++ spec/lib/import_manager_spec.rb
@@ -1,35 +1,4 @@
 require 'spec_helper'
 
 describe ImportManager do
-  let!(:pending_import) {
-    import = imports :one
-    import.update_attribute :finished_at, nil
-    ImportManager.new(import)
-  }
-
-  describe "#started?" do
-    #enqueued? = QC.default_queue.job_count()
-    before do
-      QC.delete_all
-      QC.enqueue("ImportExecutor.run", pending_import.id)
-    end
-
-    context "when the import is in the worker queue" do
-      it "should not mark the import as started" do
-        expect(pending_import).not_to be_started
-      end
-    end
-
-    context "when worker has started processing the import from the queue" do
-      let(:chorus_worker) { ChorusWorker.new :fork_worker => false }
-
-      xit "should report started" do
-        catch :running! do
-          stub(ImportExecutor).run { throw :running! }
-          chorus_worker.start
-        end
-        expect(pending_import).to be_started
-      end
-    end
-  end
 end
