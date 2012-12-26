f31e0d66da0e0cbe1efcb2a3111061fd9460f44a
Andrew Brown <pair+abrown@pivotallabs.com>
2012-12-26 14:09:18 -0800

    [#41244423] handle null schema for import datasets in import console

--- app/helpers/import_console/imports_helper.rb
+++ app/helpers/import_console/imports_helper.rb
@@ -3,8 +3,17 @@ module ImportConsole::ImportsHelper
     schema.database.name + "." + schema.name + "." + table_name
   end
 
-  def instance_description(instance)
-    instance.host + ":" + instance.port.to_s
+  def instance_description_for_schema(schema)
+    if schema
+      instance = schema.gpdb_instance
+      if instance
+        instance.host + ":" + instance.port.to_s
+      else
+        "#{schema.name} has no instance"
+      end
+    else
+      "No schema found."
+    end
   end
 
   def link_to_table(workspace, dataset)
