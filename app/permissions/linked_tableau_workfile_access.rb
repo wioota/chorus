b366362b7470eaf554fd534441fb6eff9b58d0bd
Chris Brown and Helge Holzmann <pair+cbrown+helge@pivotallabs.com>
2012-12-31 11:17:45 -0800

    [#41617119] Define update rules for access to a linked tableau workfile.

--- app/permissions/linked_tableau_workfile_access.rb
+++ app/permissions/linked_tableau_workfile_access.rb
@@ -2,4 +2,8 @@ class LinkedTableauWorkfileAccess < AdminFullAccess
   def show?(linked_tableau_workfile)
     WorkspaceAccess.new(context).can? :show, linked_tableau_workfile.workspace
   end
+
+  def update?(linked_tableau_workfile)
+    WorkspaceAccess.new(context).can? :update, linked_tableau_workfile.workspace
+  end
 end
--- spec/permissions/workfile_access_spec.rb
+++ spec/permissions/workfile_access_spec.rb
@@ -34,4 +34,30 @@ describe WorkfileAccess do
       end
     end
   end
+
+  describe "#update?" do
+    context "if the user is an admin" do
+      let(:user) { users(:admin) }
+
+      it "allows access" do
+        access.can?(:update, workfile).should be_true
+      end
+    end
+
+    context "if the user is a member of the workspace that the workfile is in" do
+      let(:user) { users(:the_collaborator) }
+
+      it "allows access" do
+        access.can?(:update, workfile).should be_true
+      end
+    end
+
+    context "if the user is not a member of the workspace that the workfile is in" do
+      let(:user) { users(:default) }
+
+      it "prevents access" do
+        access.can?(:update, workfile).should be_false
+      end
+    end
+  end
 end
