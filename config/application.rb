89a423157bb966df9ccaec13ea9b7cbb8a6f1197
Corina Lawu and Gregg Van Hove <pair+corina+gvanhove@pivotallabs.com>
2012-12-31 10:04:48 -0800

    [#38878245] Use secure cookies

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../boot', __FILE__)
+require_relative '../app/models/chorus_config'
 
 # Pick the frameworks you want:
 require "active_record/railtie"
@@ -30,6 +31,7 @@ module Chorus
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/", "#{config.root}/app/concerns"]
 
+    config.force_ssl = ChorusConfig.instance["ssl.enabled"]
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
