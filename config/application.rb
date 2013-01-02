2744e9e1f074a49d268a878468ff7e0aea2613da
Andrew Brown and Corina Lawu <pair+abrown+corina@pivotallabs.com>
2013-01-02 14:28:51 -0800

    [fixes #41244423] Precompile javascript for import console so that it works in production
    Conflicts:
    
    	app/views/import_console/imports/index.html.erb

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,7 @@ module Chorus
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w{visualizations.css import_console/import_console.css}
+    config.assets.precompile += %w{visualizations.css import_console/import_console.css import_console/import_console.js}
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
