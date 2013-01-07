42ad864559c5f62989d75e12ce2356914efa7016
Gregg Van Hove <pair+gvanhove@pivotallabs.com>
2013-01-07 10:15:06 -0800

    remove all fixtury stuff before each build so GPDB is in a known state

--- script/ci/setup.sh
+++ script/ci/setup.sh
@@ -9,7 +9,7 @@ gem list bundler | grep bundler || gem install bundler
 bundle install --binstubs=b/ || (echo "bundler failed!!!!!!!!" && exit 1)
 
 mkdir -p tmp/pids
-rm -f tmp/fixture_builder*.yml
+rm -f tmp/fixture_builder*.yml tmp/instance_integration_file_versions*.yml tmp/GPDB_HOST_STALE
 
 cp config/chorus.properties.example config/chorus.properties
 
