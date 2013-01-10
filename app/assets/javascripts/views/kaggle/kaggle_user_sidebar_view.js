0ad6ff176d060a44d1c0c95e497e968b065451c5
Andrew Brown & Luke Winikates <pair+abrown+lwinikates@pivotallabs.com>
2013-01-10 14:26:27 -0800

    [#42233095] refactor kaggle user sidebar to use MultipleSelectionSidebarMenu; also add view register and teardown to sidebar tab

--- app/assets/javascripts/views/kaggle/kaggle_user_sidebar_view.js
+++ app/assets/javascripts/views/kaggle/kaggle_user_sidebar_view.js
@@ -7,14 +7,21 @@ chorus.views.KaggleUserSidebar = chorus.views.Sidebar.extend({
     },
 
     subviews: {
-        '.tab_control': 'tabs'
+        '.tab_control': 'tabs',
+        '.multiple_selection': 'multiSelect'
     },
 
     setup: function(options) {
         this.workspace = options.workspace;
         chorus.PageEvents.subscribe("kaggleUser:selected", this.setKaggleUser, this);
         chorus.PageEvents.subscribe("kaggleUser:deselected", this.setKaggleUser, this);
-        chorus.PageEvents.subscribe("kaggleUser:checked", this.kaggleUserChecked, this);
+        this.multiSelect = new chorus.views.MultipleSelectionSidebarMenu({
+            selectEvent: "kaggleUser:checked",
+            actions: [
+                '<a class="sendMessage" data-dialog="ComposeKaggleMessage" href="#">{{t "actions.send_kaggle_message"}}</a>'
+            ]
+        });
+        this.registerSubView(this.multiSelect);
     },
 
     additionalContext: function() {
@@ -23,33 +30,24 @@ chorus.views.KaggleUserSidebar = chorus.views.Sidebar.extend({
 
     setKaggleUser: function(user) {
         this.resource = this.model = user;
+        if (this.tabs) {
+            this.tabs.teardown();
+        }
         if(user) {
             this.tabs = new chorus.views.TabControl(["information"]);
             this.tabs.information = new chorus.views.KaggleUserInformation({
                 model: user
             });
+            this.registerSubView(this.tabs);
         } else {
             this.tabs = null;
         }
         this.render();
-        this.showOrHideMultipleSelectionSection();
-    },
-
-    kaggleUserChecked: function(checkedKaggleUsers) {
-        this.checkedKaggleUsers = checkedKaggleUsers;
-        this.showOrHideMultipleSelectionSection();
-    },
-
-    showOrHideMultipleSelectionSection: function() {
-        var multiSelectEl = this.$(".multiple_selection");
-        var numChecked = this.checkedKaggleUsers ? this.checkedKaggleUsers.length : 0;
-        multiSelectEl.toggleClass("hidden", numChecked <= 1);
-        multiSelectEl.find(".count").text(t("kaggle.sidebar.multiple_selection.count", { count: numChecked }));
     },
 
     launchMultipleUserKaggleSendMessageDialog: function(e) {
         e.preventDefault();
-        new chorus.dialogs.ComposeKaggleMessage({recipients: this.checkedKaggleUsers, workspace: this.workspace}).launchModal();
+        new chorus.dialogs.ComposeKaggleMessage({recipients: this.multiSelect.selectedModels, workspace: this.workspace}).launchModal();
     },
 
     launchSingleUserKaggleSendMessageDialog: function(e) {
@@ -61,7 +59,6 @@ chorus.views.KaggleUserSidebar = chorus.views.Sidebar.extend({
     },
 
     postRender: function() {
-        this.showOrHideMultipleSelectionSection();
         this.$('.actions a.sendMessage').data('recipients', new chorus.collections.KaggleUserSet([this.model]));
         this.$('.actions a.sendMessage').data('workspace', this.workspace);
     }
--- spec/javascripts/views/kaggle/kaggle_user_sidebar_view_spec.js
+++ spec/javascripts/views/kaggle/kaggle_user_sidebar_view_spec.js
@@ -63,62 +63,29 @@ describe("chorus.views.KaggleUserSidebar", function () {
         });
     });
 
-    describe("when a single Kaggle user is checked", function () {
+    describe("when a Kaggle user is checked", function () {
         beforeEach(function () {
             this.checkedKaggleUsers = new chorus.collections.KaggleUserSet([rspecFixtures.kaggleUserSet().at(0)]);
             this.multiSelectSection = this.view.$(".multiple_selection");
             chorus.PageEvents.broadcast("kaggleUser:checked", this.checkedKaggleUsers);
         });
 
-        it("does not display the multiple selection section", function () {
-            expect(this.multiSelectSection).toHaveClass("hidden");
+        it("displays the 'send message' link", function () {
+            expect(this.multiSelectSection.find("a.sendMessage")).toContainTranslation("actions.send_kaggle_message");
         });
 
-        context("when two kaggle Users are checked", function () {
+        describe("clicking the 'send message' link", function () {
             beforeEach(function () {
-                this.checkedKaggleUsers.add(rspecFixtures.kaggleUserSet().at(1));
-                chorus.PageEvents.broadcast("kaggleUser:checked", this.checkedKaggleUsers);
+                this.modalSpy.reset();
+                this.multiSelectSection.find("a.sendMessage").click();
             });
 
-            it("does display the multiple selection section", function () {
-                expect(this.multiSelectSection).not.toHaveClass("hidden");
-            });
-
-            it("displays the number of selected kaggle Users", function () {
-                expect(this.multiSelectSection.find(".count").text()).toMatchTranslation("kaggle.sidebar.multiple_selection.count", {count:2});
-            });
-
-            it("displays the 'send message' link", function () {
-                expect(this.multiSelectSection.find("a.sendMessage")).toContainTranslation("actions.send_kaggle_message");
-            });
-
-            describe("clicking the 'send message' link", function () {
-                beforeEach(function () {
-                    this.modalSpy.reset();
-                    this.multiSelectSection.find("a.sendMessage").click();
-                });
-
-                it("launches the dialog for sending the message", function () {
-                    this.multiSelectSection.find("a.sendMessage").click();
-                    var dialog = this.modalSpy.lastModal();
-                    expect(dialog).toBeA(chorus.dialogs.ComposeKaggleMessage);
-                    expect(dialog.recipients).toBe(this.checkedKaggleUsers);
-                });
-            });
-            context("when a kaggle user is selected", function () {
-                beforeEach(function () {
-                    chorus.PageEvents.broadcast("kaggleUser:selected", rspecFixtures.kaggleUserSet().at(1));
-                });
-
-                it("should still show the multiple selection section", function () {
-                    expect(this.view.$(".multiple_selection")).not.toHaveClass("hidden");
-                });
-
-                it("should retain the selection count when the view is re-rendered", function () {
-                    expect(this.view.$(".multiple_selection .count").text()).toMatchTranslation("kaggle.sidebar.multiple_selection.count", {count:2});
-                });
+            it("launches the dialog for sending the message", function () {
+                this.multiSelectSection.find("a.sendMessage").click();
+                var dialog = this.modalSpy.lastModal();
+                expect(dialog).toBeA(chorus.dialogs.ComposeKaggleMessage);
+                expect(dialog.recipients).toBe(this.checkedKaggleUsers);
             });
         });
     });
-
 });
