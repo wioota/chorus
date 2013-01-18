2a38ce8e8e1e822ff0641cd90cd844f1298a3609
Jesse Smith & Vlad Ilin <pair+jsmith+vilin@pivotallabs.com>
2013-01-17 16:24:36 -0800

    [#42735789] Refactor integration spec for shared data source account

--- spec/integration/data_source_accounts_spec.rb
+++ spec/integration/data_source_accounts_spec.rb
@@ -5,21 +5,16 @@ describe "Data Source Permissions" do
   let(:no_access_user) { users(:default) }
   let(:owner) { users(:admin)}
 
-  it "Adds new data source account" do
+  before do
     login(owner)
     visit("#/instances/")
     find("li.instance[data-instance-id='#{the_data_source.id}']").click
     within '.account_info' do
       click_link "Edit"
     end
+  end
 
-    click_button "Add Account"
-    select_item('#select_new_instance_account_owner', no_access_user.id)
-    within "li.editing" do
-      fill_in "dbUsername", :with => "gpadmin"
-      fill_in "dbPassword", :with => "secret"
-      click_link "Save Changes"
-    end
+  after do
     click_button "Close Window"
     logout
 
@@ -29,22 +24,18 @@ describe "Data Source Permissions" do
     page.should have_selector(".breadcrumb:contains('#{the_data_source.name}')")
   end
 
-  it "Updates a Data Source to have a shared account" do
-    login(owner)
-    visit("#/instances/")
-    find("li.instance[data-instance-id='#{the_data_source.id}']").click
-    within '.account_info' do
-      click_link "Edit"
+  it "Adds new data source account" do
+    click_button "Add Account"
+    select_item('#select_new_instance_account_owner', no_access_user.id)
+    within "li.editing" do
+      fill_in "dbUsername", :with => "gpadmin"
+      fill_in "dbPassword", :with => "secret"
+      click_link "Save Changes"
     end
+  end
 
+  it "Updates a Data Source to have a shared account" do
     click_link "Switch to single shared account"
     click_button "Enable shared account"
-    click_button "Close Window"
-    logout
-
-    login(no_access_user)
-    visit("#/instances/")
-    click_link the_data_source.name
-    page.should have_selector(".breadcrumb:contains('#{the_data_source.name}')")
   end
 end
