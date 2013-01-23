c217b38ed39e16b323f46d6ec8e9707aa0e4b4cf
Corey Downing & Joseph Palermo <pair+cdowning+joseph@pivotallabs.com>
2013-01-22 17:23:00 -0800

    [#42911471] Workfiles don't break when they have no extension.

--- spec/services/workfile_name_spec.rb
+++ spec/services/workfile_name_spec.rb
@@ -5,47 +5,52 @@ describe WorkfileName do
     let(:workspace) {FactoryGirl.create :workspace }
     let(:user) {FactoryGirl.create :user }
 
-
     context "no previous workfile" do
       it "does not change the name of the file" do
         workfile = Workfile.new(:file_name => "test.sql")
         WorkfileName.resolve_name_for!(workfile)
         workfile.file_name.should == "test.sql"
       end
-
     end
 
-
     context "previous workfiles" do
-
       before do
-
         workfile = Workfile.new(:file_name => "test.sql")
         workfile.workspace = workspace
         workfile.owner = user
         workfile.save
-
       end
 
       it "changes the name of the workfile" do
-
         workfile= Workfile.new(:file_name => "test.sql")
         workfile.workspace = workspace
         WorkfileName.resolve_name_for!(workfile)
         workfile.file_name.should == "test_1.sql"
       end
+    end
 
+    context "previous workfile with no extension" do
+      before do
+        workfile = Workfile.new(:file_name => "test")
+        workfile.workspace = workspace
+        workfile.owner = user
+        workfile.save
+      end
+
+      it "changes the name of the workfile" do
+        workfile= Workfile.new(:file_name => "test")
+        workfile.workspace = workspace
+        WorkfileName.resolve_name_for!(workfile)
+        workfile.file_name.should == "test_1"
+      end
     end
 
     context "workfiles with same name in different workspace" do
-
       before do
-
         workfile = Workfile.new(:file_name => "test.sql")
         workfile.workspace = workspace
         workfile.owner = user
         workfile.save
-
       end
 
       it "does not change the name of the file" do
