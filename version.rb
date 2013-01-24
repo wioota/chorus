8123343f1227fa3b011d232def26de56fdacb611
Alex Stupakov & Callum Stott <pair+astupakov+callum@pivotallabs.com>
2013-01-24 09:53:13 -0800

    [#43073633] Update version and copyright for 2.3

--- version.rb
+++ version.rb
@@ -1,8 +1,8 @@
 module Chorus
   module VERSION #:nodoc:
     MAJOR         = 2
-    MINOR         = 2
-    SERVICE_PACK  = 1 
+    MINOR         = 3
+    SERVICE_PACK  = 0
     PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
