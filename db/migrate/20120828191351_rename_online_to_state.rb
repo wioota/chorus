c11b5e6011ca4ed869b0f40811998db5f0fbe3c1
Joseph Palermo & Luke Winikates <pair+joseph+lwinikates@pivotallabs.com>
2013-01-30 16:58:57 -0800

    {#43505537] Fix model usage in migration

--- db/migrate/20120828191351_rename_online_to_state.rb
+++ db/migrate/20120828191351_rename_online_to_state.rb
@@ -1,21 +1,23 @@
 class RenameOnlineToState < ActiveRecord::Migration
-  class Instance < ActiveRecord::Base; end
+  class MigrationInstance < ActiveRecord::Base
+    set_table_name :gpdb_instances
+  end
 
   def up
     add_column :gpdb_instances, :state, :string, :default => 'online'
-    GpdbInstance.reset_column_information
+    MigrationInstance.reset_column_information
 
-    GpdbInstance.update_all(:state => 'online')
-    GpdbInstance.where("online = false").update_all(:state => 'offline')
+    MigrationInstance.update_all(:state => 'online')
+    MigrationInstance.where("online = false").update_all(:state => 'offline')
 
     remove_column :gpdb_instances, :online
   end
 
   def down
     add_column :gpdb_instances, :online, :boolean, :default => true
-    GpdbInstance.reset_column_information
+    MigrationInstance.reset_column_information
 
-    GpdbInstance.where("state <> 'online'").update_all(:online => false)
+    MigrationInstance.where("state <> 'online'").update_all(:online => false)
 
     remove_column :gpdb_instances, :state
   end
