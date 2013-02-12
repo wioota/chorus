8c1bfa94377ef9191f8f9e4ebed0b12cb5a4c432
Charles Hansen and Corey Downing <pair+chansen+cdowning@pivotallabs.com>
2013-02-11 17:51:30 -0800

    [#44224799] Csv file presenter should only read the first 100 lines of the csv file

--- app/presenters/csv_file_presenter.rb
+++ app/presenters/csv_file_presenter.rb
@@ -2,11 +2,25 @@ class CsvFilePresenter < Presenter
   def to_hash
     {
         :id => model.id,
-        :contents => File.readlines(model.contents.path).map{ |line| line.gsub(/\n$/, '') }[0..99]
+        :contents => contents
     }
   rescue => e
     model.errors.add(:contents, :FILE_INVALID)
     raise ActiveRecord::RecordInvalid.new(model)
+  end
+
+  def contents
+    #not tested in rspec,
+    #but this needs to handle very large files without crashing the server
+    output = []
+
+    File.open(model.contents.path) do | file|
+      file.each_line do |line|
+        break if file.lineno > 100
+        output << line.gsub(/\n$/, '')
+      end
+    end
+    output
   end
 
   def complete_json?
