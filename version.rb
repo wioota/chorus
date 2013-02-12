a39f61e04314a585a9c1cb2420c5a9e4c7473969
Joseph Palermo & Luke Winikates <pair+joseph+lwinikates@pivotallabs.com>
2013-02-12 14:09:25 -0800

    [finish #44224627] Prevent paperclip from destroying symlinks

--- version.rb
+++ version.rb
@@ -2,8 +2,8 @@ module Chorus
   module VERSION #:nodoc:
     MAJOR         = 2
     MINOR         = 2
-    SERVICE_PACK  = 1
-    PATCH         = 0
+    SERVICE_PACK  = 0
+    PATCH         = 6
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
   end
 end
