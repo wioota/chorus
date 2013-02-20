b37323eb58a61d4afe0300e82bf50d3c1cb0e0a0
Andrew Brown and Corey Downing <pair+abrown+cdowning@pivotallabs.com>
2013-02-19 17:38:29 -0800

    [#39968589] add dialog-narrow class to rename tag dialog

--- app/assets/javascripts/dialogs/rename_tag_dialog.js
+++ app/assets/javascripts/dialogs/rename_tag_dialog.js
@@ -2,6 +2,7 @@ chorus.dialogs.RenameTag = chorus.dialogs.Base.extend({
     constructorName: "RenameTagDialog",
     templateName: "rename_tag",
     title: t("rename_tag.title"),
+    additionalClass: "dialog-narrow",
 
     events: {
         "submit form" : "submit",
