4c9a28098541f0ae670bbbdd7d33157d49890f9c
Callum Stott & Kris Kelly <pair+callum+kris@pivotallabs.com>
2013-02-22 09:42:08 -0800

    Update failing spec

--- spec/javascripts/views/data_tab_dataset_column_list_view_spec.js
+++ spec/javascripts/views/data_tab_dataset_column_list_view_spec.js
@@ -47,20 +47,20 @@ describe("chorus.views.DataTabDatasetColumnList", function() {
 
             describe("draggable setup", function() {
                 it("should have data-cid on the list elements", function() {
-                    expect(this.view.$('ul.list li').data('cid')).toBeTruthy();
+                    expect(this.view.$('li').data('cid')).toBeTruthy();
                 });
 
                 it("should have the fullname on the list elements", function() {
-                    expect(this.view.$('ul.list li:eq(0)').data('fullname')).toEqual("column_1");
-                    expect(this.view.$('ul.list li:eq(1)').data('fullname')).toEqual("column_2");
+                    expect(this.view.$('li:eq(0)').data('fullname')).toEqual("column_1");
+                    expect(this.view.$('li:eq(1)').data('fullname')).toEqual("column_2");
                 });
 
                 it("should make the list elements draggable", function() {
-                    expect($.fn.draggable).toHaveBeenCalledOnSelector("ul.list li");
+                    expect($.fn.draggable).toHaveBeenCalledOnSelector("li");
                 });
 
                 it("uses a draggable helper that has the name of the table", function() {
-                    var $li = this.view.$("ul.list li:eq(0)");
+                    var $li = this.view.$("li:eq(0)");
                     var helper = this.view.dragHelper({currentTarget: $li});
                     expect(helper).toHaveClass("drag_helper");
                     expect(helper).toContainText($li.data("name"));
