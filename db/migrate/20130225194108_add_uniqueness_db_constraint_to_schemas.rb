1b4e9b6c7c6373f87982bcc4bbb00934c3e47363
Callum Stott & Kris Kelly <pair+callum+kris@pivotallabs.com>
2013-02-26 15:26:41 -0800

    Revert "[#44979413] Add unique database constraint to Schema"
    This reverts commit c9f92c41863cd54cbf612cc7fde14fcdf7be40ba.

--- db/migrate/20130225194108_add_uniqueness_db_constraint_to_schemas.rb
+++
@@ -1,9 +0,0 @@
-class AddUniquenessDbConstraintToSchemas < ActiveRecord::Migration
-  def up
-    add_index :schemas, [:parent_id, :parent_type, :name], :unique => true
-  end
-
-  def down
-    remove_index :schemas, :column => [:parent_id, :parent_type, :name]
-  end
-end
