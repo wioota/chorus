6e8b2f9808a04e84091bcccb9e9ca5593d9bce20
Fiona Tay & Laura Kogler <pair+fiona+lkogler@pivotallabs.com>
2013-03-07 13:54:29 -0800

    clean up SchemaImportsController

--- app/controllers/schemas/imports_controller.rb
+++ app/controllers/schemas/imports_controller.rb
@@ -4,9 +4,9 @@ module Schemas
 
     def create
       import_params = params[:dataset_import]
-      import = SchemaImport.new(import_params)
+      schema = GpdbSchema.find(params[:schema_id])
+      import = schema.imports.new(import_params)
       import.user = current_user
-      import.schema = GpdbSchema.find(params[:schema_id])
       import.source_dataset = Dataset.find(import_params[:dataset_id])
       import.save!
 
