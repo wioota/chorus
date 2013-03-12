acad30fa01af95b050c1c3f6741c792787a66420
Callum Stott & Corey Downing <pair+callum+cdowning@pivotallabs.com>
2013-03-12 10:53:34 -0700

    [#46046253] Send SIGINT to kill workers as they respond to that

--- packaging/stop-workers.sh
+++ packaging/stop-workers.sh
@@ -9,7 +9,7 @@ bin=`cd "$bin"; pwd`
 . "$bin"/chorus-config.sh
 
 if [ -f $WORKER_PID_FILE ]; then
-  if kill -0 `cat $WORKER_PID_FILE` > /dev/null 2>&1; then
+  if kill -2 `cat $WORKER_PID_FILE` > /dev/null 2>&1; then
     log_inline "stopping workers "
     kill `cat $WORKER_PID_FILE`
     wait_for_stop $WORKER_PID_FILE
