1c3cb743ef178008de455266b8b485ecc5d150a0
Gregg Van Hove & Luke Winikates <pair+gvanhove+lwinikates@pivotallabs.com>
2013-03-22 11:17:57 -0700

    upgrade to rails 3.2.13 for security fixes, workaround regression in fake relation

--- spec/support/fake_relation.rb
+++ spec/support/fake_relation.rb
@@ -2,6 +2,6 @@ module FakeRelations
   def fake_relation(models)
     models.each { |model| model.save! }
     base_class = models.first.class.base_class
-    base_class.where(:id => models.map(&:id))
+    base_class.where("id in (#{models.map(&:id).join(',')})")
   end
 end
