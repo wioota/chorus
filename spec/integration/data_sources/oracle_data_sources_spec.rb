eae4aa42f425857db7fbfce296661575f13ac7d4
Andrew Brown & Fiona Tay <pair+abrown+fiona@pivotallabs.com>
2013-03-25 10:31:14 -0700

    unpend Oracle integration test [#43956745]

--- spec/integration/data_sources/oracle_data_sources_spec.rb
+++ spec/integration/data_sources/oracle_data_sources_spec.rb
@@ -4,6 +4,9 @@ describe "Data Source", :oracle_integration do
   include DataSourceHelpers
   include GreenplumIntegration
 
+  let(:schema) { OracleIntegration.real_schema }
+  let(:data_source) { schema.data_source }
+
   let(:user) { users(:owner) }
   before do
     login user
@@ -15,7 +18,7 @@ describe "Data Source", :oracle_integration do
       click_button "Add Data Source"
     end
 
-    it "creates a instance" do
+    it "creates a data source" do
       within_modal do
         select_and_do_within_data_source "register_existing_oracle" do
           fill_in 'name', :with => "new_oracle_data_source"
@@ -28,39 +31,31 @@ describe "Data Source", :oracle_integration do
         click_button "Add Data Source"
       end
 
-      #  See Tracker Story #42326927
       find(".data_source ul").should have_content("new_oracle_data_source")
     end
   end
 
   describe "clicking on an Oracle data source" do
-    let(:data_source) { OracleIntegration.real_data_source }
-
     before do
       visit("#/data_sources")
       click_on data_source.name
     end
 
     it "shows a list of the data source's schemas" do
-      pending "https://www.pivotaltracker.com/projects/524573/stories/43956745"
       wait_for_page_load
-      data_source.refresh_schemas.each do |schema|
-        page.should have_content schema.name
-      end
+
+      page.should have_content schema.name
     end
   end
 
   describe "clicking on an Oracle schema" do
-    let(:schema) { OracleIntegration.real_schema }
-    let(:data_source) { schema.data_source }
-
     before do
       schema.datasets.size.should > 0
       visit("#/data_sources/#{data_source.id}/schemas")
       find(:xpath, "//a[text()='#{schema.name}']").click
     end
 
-    it "should show a list of the datasets in the schema" do
+    it "shows a list of the datasets in the schema" do
       wait_for_page_load
       schema.datasets.each do |dataset|
         page.should have_content dataset.name
