b5be14eaa715e7c2ce50319f949359c700a3a230
Callum Stott & Gregg Van Hove <pair+callum+gvanhove@pivotallabs.com>
2013-03-26 10:33:52 -0700

    don't use transactions for dropping and creating databases... oops

--- spec/support/database_integration/drop_and_create_gpdb_databases.sql
+++ spec/support/database_integration/drop_and_create_gpdb_databases.sql
@@ -1,13 +1,10 @@
-BEGIN;
 DROP DATABASE IF EXISTS "gpdb_test_database";
 DROP DATABASE IF EXISTS "gpdb_test_database_priv";
 DROP DATABASE IF EXISTS "gpdb_test_database_with_''_";
 DROP DATABASE IF EXISTS "gpdb_test_database_wo_pub";
 DROP ROLE IF EXISTS "user_with_restricted_access";
 -- DROP DATABASE IF EXISTS "gpdb_test_database_with_""_";
-COMMIT;
 
-BEGIN;
 CREATE ROLE "user_with_restricted_access" PASSWORD 'secret';
 CREATE DATABASE "gpdb_test_database" OWNER gpadmin;
 REVOKE CONNECT ON DATABASE "gpdb_test_database" FROM PUBLIC;
@@ -15,4 +12,3 @@ CREATE DATABASE "gpdb_test_database_priv" OWNER gpadmin;
 CREATE DATABASE "gpdb_test_database_with_''_" OWNER gpadmin;
 CREATE DATABASE "gpdb_test_database_wo_pub" OWNER gpadmin;
 -- CREATE DATABASE "gpdb_test_database_with_""_" OWNER gpadmin;
-COMMIT;
