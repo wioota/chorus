602440a92681aeb4865c75927fc070da122a7be6
Callum Stott & Gregg Van Hove <pair+callum+gvanhove@pivotallabs.com>
2013-03-26 11:51:29 -0700

    hide output from ipv6 detection

--- vendor/nginx/generate_nginx_conf.rb
+++ vendor/nginx/generate_nginx_conf.rb
@@ -8,7 +8,7 @@ destination_path = chorus_home + '/vendor/nginx/nginx_dist/nginx_data/conf'
 nginx_template_path = chorus_home + '/vendor/nginx/nginx.conf.erb'
 chorus_config = ChorusConfig.new chorus_home
 rails_env = ENV['RAILS_ENV']
-ip6_enabled = system("/sbin/ifconfig | grep inet6")
+ip6_enabled = system("/sbin/ifconfig | grep inet6 > /dev/null")
 
 template = ERB.new File.read(nginx_template_path)
 
