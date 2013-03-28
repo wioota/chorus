ac44b3bd9f49980bd1fb97f66913ac2c3e74bb6c
Andrew Brown <pair+abrown@pivotallabs.com>
2013-03-28 16:50:13 -0700

    include asset pipeline for ci_legacy builds

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test integration ci_jasmine)))
+  Bundler.require(*Rails.groups(:assets => %w(development test integration ci_jasmine ci_legacy)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
