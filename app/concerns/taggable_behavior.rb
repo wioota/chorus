9548a4552e84b181635e9c8db0aac06136fdf74b
Andrew Brown & George Dean <pair+abrown+gdean@pivotallabs.com>
2013-04-03 11:01:05 -0700

    [#47379699] Don't bother to handle database-level exceptions on tag creation.

--- app/concerns/taggable_behavior.rb
+++ app/concerns/taggable_behavior.rb
@@ -14,11 +14,7 @@ module TaggableBehavior
 
   def tag_list=(tags_list)
     self.tags = tags_list.map do |tag_name|
-      begin
-        Tag.create!(name: tag_name)
-      rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid => e # find_or_create_by_name is not concurrent-safe
-        Tag.first(:conditions => [ "lower(name) = ?", tag_name.downcase ])
-      end
+      Tag.find_or_create_by_tag_name(tag_name)
     end
   end
 end
