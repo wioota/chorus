8d913d73489da634a40f91aa4da2d583742cc784
Joseph Palermo <pair+joseph@pivotallabs.com>
2013-04-15 13:32:58 -0700

    Clean up workspaces integration specs

--- spec/integration/workspaces_spec.rb
+++ spec/integration/workspaces_spec.rb
@@ -8,21 +8,6 @@ describe "Workspaces" do
   let(:workspace) { workspaces(:public) }
 
   describe "Create workspaces" do
-    it "creates a private workspace" do
-      visit('#/workspaces')
-      click_button "Create Workspace"
-      within_modal do
-        fill_in 'name', :with => "New Workspace"
-        uncheck "Make this workspace publicly available"
-        click_button "Create Workspace"
-      end
-      click_link "Dismiss the workspace quick start guide"
-      logout
-      login(users(:no_collaborators))
-      visit('#/workspaces')
-      page.should_not have_content ("Private Workspace")
-    end
-
     it "creates a public workspace" do
       visit('#/workspaces')
       click_button "Create Workspace"
@@ -48,20 +33,14 @@ describe "Workspaces" do
   end
 
   describe "Delete a workspace" do
-
     it "deletes the workspace" do
       visit("#/workspaces/#{workspace.id}")
       click_link "Delete this Workspace"
-      page.should have_selector(".submit")
-      find(".submit").click
+      click_button "Delete Workspace"
 
+      wait_for_page_load
       visit('/#/workspaces')
-      within '.content_footer' do
-        page.should have_no_selector(".loading_section")
-      end
 
-      find('a', :text => "Active Workspaces", :visible => true).click
-      find('a', :text => "All Workspaces", :visible => true).click
       within ".list" do
         page.should have_css("a[href='#/workspaces/#{Workspace.first.id}']")
         page.should_not have_css("a[href='#/workspaces/#{workspace.id}']")
