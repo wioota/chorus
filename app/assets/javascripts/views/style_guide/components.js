30c3f998d519c3490de8efa8b7c3f17b3e16cf4e
Charles Hansen & Nicole Sullivan <pair+chansen+nsullivan@pivotallabs.com>
2013-04-22 14:06:19 -0700

    [#47683669] Add link component and docs.

--- app/assets/javascripts/views/style_guide/components.js
+++ app/assets/javascripts/views/style_guide/components.js
@@ -9,11 +9,16 @@ chorus.views.Components = chorus.views.Base.extend({
         "<h4>h4.heading level four</h4>",
         "<h5>h5.heading level five</h5>",
         "<h6>h6.heading level six</h6>",
-        "<p>Standard paragraphy text. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>",
         "<h5 class='h2'>h5 with h2 class</h5>",
-        '<p class="low_light">Low light text</p>',
+        "<p>Standard paragraphy text. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>",
         '<p class="deemphasized">deemphasized text</p>',
+        '<p class="low_light">Low light text</p>',
         '<strong>Strong text!!!</strong>'
+    ],
+
+    links: [
+        "<a href='#'>default link</a>",
+        "<a class='link_low_light' href='#'>lowlight link</a>"
     ],
 
     postRender: function() {
@@ -22,7 +27,8 @@ chorus.views.Components = chorus.views.Base.extend({
 
     additionalContext: function() {
         return {
-            typographies: this.typographies
+            typographies: this.typographies,
+            links: this.links
         };
     }
 });
