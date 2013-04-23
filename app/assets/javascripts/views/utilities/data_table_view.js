727ba9e6186b57155f97a68054cb3f980cdf553c
Luke Winikates & Nicole Sullivan <pair+lwinikates+nsullivan@pivotallabs.com>
2013-04-23 14:32:11 -0700

    [#42309973] remove the 'move column left' button from the data preview window

--- app/assets/javascripts/views/utilities/data_table_view.js
+++ app/assets/javascripts/views/utilities/data_table_view.js
@@ -2,31 +2,9 @@ chorus.views.DataTable = chorus.views.Base.extend({
     templateName: "data_table",
     constructorName: "DataTable",
 
-    events: {
-        "click a.move_to_first": "moveColumnToFirst"
-    },
-
     // backbone events don't work for scroll?!
     postRender: function() {
         this.$(".tbody").bind("scroll", _.bind(this.adjustHeaderPosition, this));
-        this.$("a.move_to_first").qtip({
-            content: {
-                text: t("results_console_view.data_table.move_to_first_column")
-            },
-            hide: 'mouseleave click',
-            style: {
-                classes: "tooltip-help",
-                tip: {
-                    width: 12,
-                    height: 12
-                }
-            },
-            position: {
-                my: "bottom center",
-                at: "top center",
-                container: this.el
-            }
-        });
 
         this.setupScrolling(".tbody");
         this.setupResizability();
@@ -54,17 +32,5 @@ chorus.views.DataTable = chorus.views.Base.extend({
     scrollLeft: function() {
         var api = this.$(".tbody").data("jsp");
         return api && api.getContentPositionX();
-    },
-
-    moveColumnToFirst: function(e) {
-        e.preventDefault();
-
-        var $th = $(e.currentTarget).closest(".th");
-        var $thead = this.$(".thead");
-        var $tbody = this.$(".column:first").parent();
-        var index = $thead.find(".th").index($th);
-
-        $thead.prepend($th);
-        $tbody.prepend(this.$(".column").eq(index));
     }
 });
--- spec/javascripts/views/utilities/data_table_view_spec.js
+++ spec/javascripts/views/utilities/data_table_view_spec.js
@@ -44,10 +44,6 @@ describe("chorus.views.DataTable", function() {
             this.view.render();
         });
 
-        it("should enable shuttling", function() {
-            expect(this.view.$(".th a.move_to_first").length).not.toBe(0);
-        });
-
         it("renders a column div for every column of the result", function() {
             expect(this.view.$("div.column").length).toBe(4);
         });
@@ -86,53 +82,6 @@ describe("chorus.views.DataTable", function() {
 
         it("sets up custom scrolling", function() {
             expect($.fn.jScrollPane).toHaveBeenCalled();
-        });
-
-        context("clicking on the jump to left arrow", function() {
-            beforeEach(function() {
-                this.view.$(".th:eq(2) a.move_to_first").click();
-            });
-
-            it("moves the clicked column to the leftmost position", function() {
-                expect(this.view.$("div.th:eq(0) .name").text()).toBe("state");
-                expect(this.view.$("div.th:eq(1) .name").text()).toBe("id");
-                expect(this.view.$("div.th:eq(2) .name").text()).toBe("city");
-                expect(this.view.$("div.th:eq(3) .name").text()).toBe("zip");
-
-                expect(this.view.$(".column:eq(0) div.td:eq(0)").text()).toBe("CA");
-                expect(this.view.$(".column:eq(1) div.td:eq(0)").text()).toBe("1");
-                expect(this.view.$(".column:eq(2) div.td:eq(0)").text()).toBe("Oakland");
-                expect(this.view.$(".column:eq(3) div.td:eq(0)").text()).toBe("94612");
-            });
-
-            it("keeps all the columns and headers at the same level", function() {
-                expect(this.view.$("div.th:eq(0)").parent().children('.th').length).toBe(4);
-                expect(this.view.$(".column:eq(0)").parent().children('.column').length).toBe(4);
-            });
-        });
-
-        describe("enable and disable shuttling", function() {
-            context("when shuttling is enabled", function() {
-                beforeEach(function() {
-                    this.view.options.shuttle = true;
-                    this.view.render();
-                });
-
-                it("should enable shuttling", function() {
-                    expect(this.view.$(".th a.move_to_first").length).not.toBe(0);
-                });
-            });
-
-            context("when shuttling is disabled", function() {
-                beforeEach(function() {
-                    this.view.options.shuttle = false;
-                    this.view.render();
-                });
-
-                it("should disable shuttling", function() {
-                    expect(this.view.$(".th a.move_to_first").length).toBe(0);
-                });
-            });
         });
     });
 });
