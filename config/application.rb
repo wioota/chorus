f099e86e8dec3db38ab52039486cb4ed15765d0f
Gavin Morgan and Luke Winikates <pair+gmorgan+lwinikates@pivotallabs.com>
2013-05-08 11:23:49 -0700

    Lose the fancy unicode delimiter

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w•development test integration ci_jasmine ci_legacy ci_next•))
+  Bundler.require(*Rails.groups(:assets => %w|development test integration ci_jasmine ci_legacy ci_next|))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
