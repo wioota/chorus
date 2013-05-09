5d5b5375884ba41c739790a2955222efa13a7a05
Gavin Morgan & Joseph Palermo <pair+gmorgan+joseph@pivotallabs.com>
2013-05-09 10:23:01 -0700

    [#47553665] Clean orphans

--- app/services/orphan_cleaner.rb
+++ app/services/orphan_cleaner.rb
@@ -3,5 +3,21 @@ class OrphanCleaner
     HdfsEntry.where("hdfs_data_source_id IN (SELECT id FROM hdfs_data_sources WHERE deleted_at IS NOT NULL)").find_each do |entry|
       entry.destroy
     end
+
+    GpdbDatabase.where("data_source_id IN (SELECT id FROM data_sources WHERE deleted_at IS NOT NULL)").find_each do |database|
+      database.destroy
+    end
+
+    GpdbSchema.where("parent_id IN (SELECT id FROM gpdb_databases WHERE deleted_at IS NOT NULL)").find_each do |schema|
+      schema.destroy
+    end
+
+    OracleSchema.where("parent_id IN (SELECT id FROM data_sources WHERE type = 'OracleDataSource' AND deleted_at IS NOT NULL)").find_each do |schema|
+      schema.destroy
+    end
+
+    Dataset.where("schema_id IN (SELECT id FROM schemas WHERE deleted_at IS NOT NULL)").find_each do |dataset|
+      dataset.destroy
+    end
   end
 end
--- spec/services/orphan_cleaner_spec.rb
+++ spec/services/orphan_cleaner_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe OrphanCleaner do
   describe "clean" do
+    before do
+      any_instance_of(GreenplumConnection) do |connection|
+        stub(connection).running?
+      end
+    end
+
     it "removes orphaned hdfs entries" do
       hdfs_data_source = hdfs_data_sources(:hadoop)
       entries = hdfs_data_source.hdfs_entries
@@ -9,6 +15,57 @@ describe OrphanCleaner do
       hdfs_data_source.destroy
       OrphanCleaner.clean
       entries.count.should == 0
+    end
+
+    context "for gpdb data source" do
+      let(:data_source) { data_sources(:owners) }
+
+      it "removes orphaned gpdb databases" do
+        databases = data_source.databases
+        data_source.destroy
+        databases.count.should > 0
+        OrphanCleaner.clean
+        databases.count.should == 0
+      end
+
+      it "removes orphaned schemas" do
+        schema_ids = data_source.schema_ids
+        data_source.destroy
+        data_source.databases.update_all(:deleted_at => Time.now)
+        schema_ids.length.should > 0
+        OrphanCleaner.clean
+        Schema.where(:id => schema_ids).count.should == 0
+      end
+
+      it "removes orphaned datasets" do
+        dataset_ids = data_source.dataset_ids
+        data_source.destroy
+        data_source.schemas.update_all(:deleted_at => Time.now)
+        dataset_ids.length.should > 0
+        OrphanCleaner.clean
+        Dataset.where(:id => dataset_ids).count.should == 0
+      end
+    end
+
+    context "for oracle source" do
+      let(:data_source) { data_sources(:oracle) }
+
+      it "removes orphaned schemas" do
+        schemas = data_source.schemas
+        data_source.destroy
+        schemas.length.should > 0
+        OrphanCleaner.clean
+        schemas.count.should == 0
+      end
+
+      it "removes orphaned datasets" do
+        dataset_ids = data_source.dataset_ids
+        data_source.destroy
+        data_source.schemas.update_all(:deleted_at => Time.now)
+        dataset_ids.length.should > 0
+        OrphanCleaner.clean
+        Dataset.where(:id => dataset_ids).count.should == 0
+      end
     end
   end
 end
