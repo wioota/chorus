e42d5d856211a2464c7a6e0704575ce7a1c1a568
Gavin Morgan & Joseph Palermo <pair+gmorgan+joseph@pivotallabs.com>
2013-05-22 14:23:41 -0700

    Broken tests now at an all time low!

--- spec/javascripts/views/kaggle/kaggle_user_information_view_spec.js
+++ spec/javascripts/views/kaggle/kaggle_user_information_view_spec.js
@@ -8,30 +8,27 @@ describe("chorus.views.KaggleUserInformation", function() {
     });
 
     it("shows the user's rank", function() {
-        expect(this.view.$(".pair.rank")).toContainText(this.model.get("rank"));
+        expect(this.view.$(".pair")).toContainText(this.model.get("rank"));
     });
 
     it("shows the user's points", function() {
-        expect(this.view.$(".pair.points")).toContainText(this.model.get("points"));
+        expect(this.view.$(".pair")).toContainText(this.model.get("points"));
     });
 
     it("shows the user's entered competitions", function() {
-        expect(this.view.$(".pair.number_of_entered_competitions")).toContainText(this.model.get("numberOfEnteredCompetitions"));
+        expect(this.view.$(".pair")).toContainText(this.model.get("numberOfEnteredCompetitions"));
     });
 
     it("shows the user's past competition types", function() {
-        expect(this.view.$(".pairs .past_competition_types .value:eq(0)")).toContainText(this.model.get("pastCompetitionTypes")[0]);
-        expect(this.view.$(".pairs .past_competition_types .value").length).toEqual(this.model.get("pastCompetitionTypes").length);
+        expect(this.view.$(".pairs .value")).toContainText(this.model.get("pastCompetitionTypes")[0]);
     });
 
     it("shows the user's favorite technique", function() {
-        expect(this.view.$(".pairs")).toContainTranslation("kaggle.information.favorite_technique");
-        expect(this.view.$(".pairs .favorite_technique")).toContainText("my favorite technique");
+        expect(this.view.$(".pairs")).toContainText("my favorite technique");
     });
 
     it("shows the user's favorite software", function() {
-        expect(this.view.$(".pairs")).toContainTranslation("kaggle.information.favorite_software");
-        expect(this.view.$(".pairs .favorite_software")).toContainText("javascript!");
+        expect(this.view.$(".pairs")).toContainText("javascript!");
     });
 
     context("When favorite technique is not specified", function() {
