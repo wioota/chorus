d8b70071e32588c72667043028cf8cfda39e477d
James Myers & Kris Kelly <pair+jmyers+kris@pivotallabs.com>
2013-06-03 11:24:56 -0700

    [#49625417] Handle form validation and errors for rename workfile dialog

--- app/assets/javascripts/dialogs/workfiles/rename_workfile_dialog.js
+++ app/assets/javascripts/dialogs/workfiles/rename_workfile_dialog.js
@@ -1,16 +1,29 @@
-chorus.dialogs.RenameWorkfile = chorus.dialogs.Base.extend({
+chorus.dialogs.RenameWorkfile = chorus.dialogs.Base.include(chorus.Mixins.DialogFormHelpers).extend({
     constructorName: "RenameWorkfileDialog",
     templateName: "rename_workfile_dialog",
 
     title: t("workfile.rename_dialog.title"),
 
-    events: {
-        "click button.submit": "submit"
+    setup: function() {
+        this.listenTo(this.model, "saved", this.saved);
+        this.listenTo(this.model, "saveFailed", this.saveFailed);
+        this.disableFormUnlessValid({
+            formSelector: "form",
+            inputSelector: "input"
+        });
     },
 
-    submit: function(e) {
-        e && e.preventDefault();
-        this.model.set({fileName: this.$("input").val()});
+    create: function(e) {
+        this.model.set({fileName: this.$("input").val()}, {silent: true});
         this.model.save();
+        this.$("button.submit").startLoading("actions.renaming");
+    },
+
+    saved: function() {
+        this.closeModal();
+    },
+
+    saveFailed: function() {
+        this.$("button.submit").stopLoading();
     }
 });
--- spec/javascripts/dialogs/workfiles/rename_workfile_dialog_spec.js
+++ spec/javascripts/dialogs/workfiles/rename_workfile_dialog_spec.js
@@ -13,10 +13,59 @@ describe("chorus.dialogs.RenameWorkfile", function() {
        expect(this.dialog.$('input').val()).toBe("originalName.sql");
     });
 
-    it("should update the workfile when clicking submit", function() {
-        this.dialog.$('input').val("newName.sql").change();
-        this.dialog.$("button.submit").click();
-        expect(this.workfile.get("fileName")).toBe("newName.sql");
-        expect(this.workfile).toHaveBeenUpdated();
+    context("with invalid form values", function() {
+        beforeEach(function() {
+            this.dialog.$('input').val("").keyup();
+        });
+
+        it("does not let you submit the form", function() {
+            expect(this.dialog.$("button.submit")).toBeDisabled();
+        });
+    });
+
+    context("when submitting the form", function() {
+        beforeEach(function() {
+            spyOn(this.dialog, "closeModal");
+            spyOnEvent(this.dialog.model, "change");
+            this.dialog.$('input').val("newName.sql").change();
+            this.dialog.$("form").submit();
+        });
+
+        it("should not change the fileName elsewhere until the save completes", function() {
+            expect("change").not.toHaveBeenTriggeredOn(this.dialog.model);
+        });
+
+        it("should update the workfile", function() {
+            expect(this.workfile.get("fileName")).toBe("newName.sql");
+            expect(this.workfile).toHaveBeenUpdated();
+        });
+
+        it("should be loading", function() {
+            expect(this.dialog.$("button.submit").isLoading()).toBeTruthy();
+        });
+
+        context("when the save is successful", function() {
+            beforeEach(function() {
+                this.server.lastUpdate().succeed();
+            });
+
+            it("closes the modal", function() {
+                expect(this.dialog.closeModal).toHaveBeenCalled();
+            });
+
+            it("re-renders", function() {
+                expect("change").toHaveBeenTriggeredOn(this.dialog.model);
+            });
+        });
+
+        context("when the save fails", function() {
+            beforeEach(function() {
+                this.server.lastUpdate().failUnprocessableEntity();
+            });
+
+            it("stops the loading spinner", function() {
+                expect(this.dialog.$("button.submit").isLoading()).toBeFalsy();
+            });
+        });
     });
 });
