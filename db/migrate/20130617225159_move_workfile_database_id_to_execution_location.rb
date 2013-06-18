c6997767c4b98fb9b72bed23155e00c0b46ce455
Gavin Morgan & Luke Winikates <pair+gmorgan+lwinikates@pivotallabs.com>
2013-06-18 11:41:23 -0700

    [#50832459] Augment migration to skip validations & callbacks.
    - Also saves properly unparsed JSON.

--- db/migrate/20130617225159_move_workfile_database_id_to_execution_location.rb
+++ db/migrate/20130617225159_move_workfile_database_id_to_execution_location.rb
@@ -10,17 +10,20 @@ class MoveWorkfileDatabaseIdToExecutionLocation < ActiveRecord::Migration
       database_id = wf.additional_data['database_id']
       if database_id && wf.execution_location_id.nil?
         p "Setting AlpineWorkfile #{wf.id} execution_location to GpdbDatabase #{database_id}."
-        wf.execution_location_id = database_id
+        wf.update_column(:execution_location_id, database_id)
         wf.additional_data.delete 'database_id'
-        wf.save!
+        wf.update_column(:additional_data, JSON.unparse(wf.additional_data))
       end
     end
   end
 
   def down
     MigrationWorkfile.where(type: 'AlpineWorkfile').find_each do |wf|
-      wf.additional_data['database_id'] = wf.execution_location_id if wf.execution_location_id
-      wf.save!
+      p "Storing AlpineWorkfile #{wf.id} execution_location in additional_data."
+      if wf.execution_location_id
+        wf.additional_data['database_id'] = wf.execution_location_id
+        wf.update_column(:additional_data, JSON.unparse(wf.additional_data))
+      end
     end
   end
 end
