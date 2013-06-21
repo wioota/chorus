56d23f6e5a3e3757031e3557b858d66871523c60
Kris Kelly & Luke Winikates <pair+kris+lwinikates@pivotallabs.com>
2013-06-21 11:25:56 -0700

    [#51188533] move Alpine Workfile Validator into its own file

--- app/models/alpine_workfile.rb
+++ app/models/alpine_workfile.rb
@@ -1,27 +1,6 @@
 class AlpineWorkfile < Workfile
   TooManyDataBases = Class.new(StandardError)
 
-  class AlpineWorkfileValidator < ActiveModel::Validator
-    def validate(record)
-      ensure_single_database(record)
-      ensure_no_chorus_views(record)
-      ensure_active_workspace(record)
-    end
-
-    def ensure_single_database(record)
-      record_datasets_map = record.datasets.map(&:database)
-      record.errors[:datasets] << :too_many_databases unless record_datasets_map.uniq.count <= 1
-    end
-
-    def ensure_no_chorus_views(record)
-      record.errors[:datasets] << :chorus_view_selected if record.datasets.map(&:type).include?("ChorusView")
-    end
-
-    def ensure_active_workspace(record)
-      record.errors[:workspace] << :ARCHIVED if record.workspace && record.workspace.archived?
-    end
-  end
-
   has_additional_data :dataset_ids
 
   before_validation { self.content_type ='work_flow' }
