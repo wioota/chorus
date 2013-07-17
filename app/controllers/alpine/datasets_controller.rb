e5bc80bd185f89655dc999b087567c2414a2ceb9
Gavin Morgan and Robbie Gill <pair+gmorgan+robbie@pivotallabs.com>
2013-07-17 14:43:38 -0700

    Removes unused secondary dataset API

--- app/controllers/alpine/datasets_controller.rb
+++
@@ -1,8 +0,0 @@
-module Alpine
-  class DatasetsController < AlpineController
-    def index
-      datasets = Dataset.includes(Dataset.eager_load_succinct_associations).find(params[:dataset_ids])
-      present datasets, :presenter_options => { :succinct => true }
-    end
-  end
-end
