e56a4c658b5a90f0306e7dd52720c1e9905e2ea3
Robbie Gill and Travis Grathwell <pair+robbie+tgrathwell@pivotallabs.com>
2013-07-17 17:32:50 -0700

    [#53516641] Hdfs Datasets do not show "Import" and "Add workspace" links in the sidebar.
    Fix migration to use appropriate class names when shadowing models

--- db/migrate/20130717175011_populate_workspace_id_for_hdfs_datasets.rb
+++ db/migrate/20130717175011_populate_workspace_id_for_hdfs_datasets.rb
@@ -1,9 +1,13 @@
 class PopulateWorkspaceIdForHdfsDatasets < ActiveRecord::Migration
   class Dataset < ActiveRecord::Base; end
-  class AssociatedDatabase < ActiveRecord::Base; end
+  class HdfsDataset < Dataset; end
+  class AssociatedDataset < ActiveRecord::Base;
+    belongs_to :workspace
+    belongs_to :dataset
+  end
 
   def up
-    Dataset.where(type: 'HdfsDataset').find_each do |dataset|
+    HdfsDataset.find_each do |dataset|
       associations = AssociatedDataset.where(dataset_id: dataset.id)
       if associations.present?
         dataset.update_attribute(:workspace_id, associations.first.workspace.id)
