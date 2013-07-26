63f798a7189a055b36b1766ed5f5d5e33e45fc96
James Myers and Robbie Gill <pair+jmyers+robbie@pivotallabs.com>
2013-07-26 10:20:26 -0700

    [#53741443] User can edit a job.
    - Refactor and fix issue with am/pm.

--- app/assets/javascripts/dialogs/jobs/edit_job_dialog.js
+++ app/assets/javascripts/dialogs/jobs/edit_job_dialog.js
@@ -6,29 +6,16 @@ chorus.dialogs.EditJob = chorus.dialogs.ConfigureJob.include(chorus.Mixins.Dialo
 
     setup: function () {
         this.model = this.job =  this.options.model;
-        var startDate = this.model.get("nextRun");
-        this.startDate = startDate ? new Date(startDate) : new Date();
+        this.startDate = this.model.nextRunDate();
+        this.endDate = this.model.endRunDate();
 
-        var endDate = this.model.get("endRun");
-        this.endDate = endDate ? new Date(endDate) : new Date();
         this._super('setup');
     },
 
     postRender: function () {
         this._super("postRender");
 
-        this.$('input.name').val(this.model.get('name'));
-        this.$('input.end_date_enabled').prop("checked", this.model.get("endRun")).trigger("change");
-
-        if(!this.model.runsOnDemand()) {
-            this.$('input:radio#onDemand').prop("checked", false);
-            this.$('input:radio#onSchedule').prop("checked", true).trigger("change");
-
-            this.$('input.interval_value').val(this.model.get('intervalValue'));
-            this.$('select.interval_unit').val(this.model.get('intervalUnit'));
-
-            this.populateExistingTime();
-        }
+        this.prePopulateFields();
         this.toggleSubmitDisabled();
     },
 
@@ -41,19 +28,31 @@ chorus.dialogs.EditJob = chorus.dialogs.ConfigureJob.include(chorus.Mixins.Dialo
     },
 
     populateExistingTime: function () {
-        var hoursBase = this.startDate.getHours();
-        var meridian = hoursBase - 11 > 0 ? "pm" : "am";
-        var hours = meridian === "pm" ? hoursBase - 12 : hours;
-        hours = hours === 0 ? 12 : hours;
+        var times = this.model.nextRunTime();
 
-        this.$('select.hour').val(hours);
-        this.$('select.minute').val(Math.floor(this.startDate.getMinutes() / 5) * 5);
-        this.$('select.meridian').val(meridian);
+        this.$('select.hour').val(times.hours);
+        this.$('select.minute').val(times.minutes);
+        this.$('select.meridian').val(times.meridian);
     },
 
     modelSaved: function () {
         chorus.toast(this.message);
         this.model.trigger('invalidated');
         this.closeModal();
+    },
+
+    prePopulateFields: function () {
+        this.$('input.name').val(this.model.get('name'));
+        this.$('input.end_date_enabled').prop("checked", this.model.get("endRun")).trigger("change");
+
+        if (!this.model.runsOnDemand()) {
+            this.$('input:radio#onDemand').prop("checked", false);
+            this.$('input:radio#onSchedule').prop("checked", true).trigger("change");
+
+            this.$('input.interval_value').val(this.model.get('intervalValue'));
+            this.$('select.interval_unit').val(this.model.get('intervalUnit'));
+
+            this.populateExistingTime();
+        }
     }
 });
--- app/assets/javascripts/models/job.js
+++ app/assets/javascripts/models/job.js
@@ -19,5 +19,29 @@ chorus.models.Job = chorus.models.Base.extend({
 
     runsOnDemand: function () {
         return this.get("intervalUnit") === "on_demand";
+    },
+
+    nextRunDate: function () {
+        var startDate = this.get('nextRun');
+        return startDate ? new Date(startDate) : new Date();
+    },
+
+    endRunDate: function () {
+        var endDate = this.get('endRun');
+        return endDate ? new Date(endDate) : new Date();
+    },
+
+    nextRunTime: function () {
+        var hoursBase = this.nextRunDate().getHours();
+        var meridian = hoursBase - 11 > 0 ? "pm" : "am";
+        var hours = meridian === "pm" ? hoursBase - 12 : hoursBase;
+        var minutes = Math.floor(this.nextRunDate().getMinutes() / 5) * 5;
+        hours = hours === 0 ? 12 : hours;
+
+        return {
+            hours: hours,
+            minutes: minutes,
+            meridian: meridian
+        };
     }
 });
