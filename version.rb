4baf9aedba955b443ab311a03d817e4347ba10b6
Gavin Morgan <pair+gavin@pivotallabs.com>
2013-09-13 10:03:32 -0700

    bump service_pack version

--- version.rb
+++ version.rb
@@ -2,8 +2,8 @@ module Chorus
   module VERSION #:nodoc:
     MAJOR         = 2
     MINOR         = 5
-    SERVICE_PACK  = 0
-    PATCH         = 1
+    SERVICE_PACK  = 2
+    PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
   end
