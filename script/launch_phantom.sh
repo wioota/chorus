1207d15ef412a111a780e0ca4632b528b703e7a9
Robbie Gill and Travis Grathwell <pair+robbie+travis@pivotallabs.com>
2013-10-03 16:23:44 -0700

    Add script to run JS tests in phantom (bind to editor's external tools to launch)

---
+++ script/launch_phantom.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Opening $JASMINE_URL to run all the tests"
+    open "$JASMINE_URL"
+    exit 0
+fi
+
+# is this a spec file?
+echo $1 | grep 'spec.js$'
+
+if [ $? -eq 0 ]; then
+  # ends with 'spec.js', probably a spec file
+  SPECFILE=$1
+else
+  # inject _spec and find the file with the right name
+  SPECFILE=$(find ./ofbiz/hot-deploy/client/spec -type f -iname $(basename $1 | sed 's/.js$/_spec.js/'))
+  if [ -z "$SPECFILE" ]; then
+    echo "Could not locate a spec file to go with $1" >&2
+    exit 1
+  fi
+
+  echo "Detected $SPECFILE as matching spec for $1"
+fi
+
+DESCRIBE_LINE=$(grep describe $SPECFILE | head -n1)
+
+if [ -z "$DESCRIBE_LINE" ]; then
+    echo "This does not appear to be a spec file." >&2
+    exit 1
+fi
+
+# to escape the single quotes in sed, you do: '\'' - escaping your single quotes, and wrapping that in single quotes
+# see: http://muffinresearch.co.uk/archives/2007/01/30/bash-single-quotes-inside-of-single-quoted-strings/
+SPEC=$(echo $DESCRIBE_LINE | sed 's/.*["'\'']\(.*\)["'\''][^"'\'']*$/\1/')
+
+echo "Running $JASMINE_URL$SPEC in phantom"
+
+phantomjs /Users/pivotal/workspace/chorusrails/lib/tasks/../../spec/run-phantom-jasmine.js 8888 $SPEC
