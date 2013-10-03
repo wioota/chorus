470192d5d03783df8372b892612ead3f63bb8f95
Gavin Morgan and Jason Lee <pair+gavin+jason@pivotallabs.com>
2013-10-03 09:22:36 -0700

    Packaging respects whitelabeling Set PIVOTALLABEL environment variable to true before running packaging.sh

--- script/ci/packaging.sh
+++ script/ci/packaging.sh
@@ -11,5 +11,9 @@ if [[ $ALPINE_ZIP ]]; then
     (mkdir -p vendor/alpine; cd vendor/alpine; wget --quiet $ALPINE_ZIP)
 fi
 
+if [[ $PIVOTALLABEL ]]; then
+    sed -i .og "s/alpine\.branded\.enabled\=true/alpine\.branded\.enabled\=false/" config/chorus.defaults.properties
+fi
+
 rm -fr .bundle
 GPDB_HOST=$GPDB_HOST HAWQ_HOST=$HAWQ_HOST ORACLE_HOST=$ORACLE_HOST bundle exec rake package:installer --trace
