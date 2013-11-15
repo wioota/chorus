b77d58b09d0252d9da3f0bff3717ac0f997aaa1c
Robbie Gill <rgill@alpinenow.com>
2013-11-15 11:37:09 -0800

    Begin development of 3.1.0.0

--- version.rb
+++ version.rb
@@ -1,8 +1,8 @@
 module Chorus
   module VERSION #:nodoc:
     MAJOR         = 3
-    MINOR         = 0
-    SERVICE_PACK  = 1
+    MINOR         = 1
+    SERVICE_PACK  = 0
     PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
