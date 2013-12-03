e7a624d0163d7ba19150f4651ac9484a2cd6c5a1
Robbie Gill <rgill@alpinenow.com>
2013-12-03 12:59:06 -0800

    Enables request forgery_protection for integration specs.

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -4,4 +4,5 @@ Chorus::Application.configure do
   config.action_dispatch.show_exceptions = false # make sure capybara server middleware gets exceptions
   config.middleware.delete(::Rack::Sendfile)
   config.allow_concurrency = true
+  config.action_controller.allow_forgery_protection = true
 end
