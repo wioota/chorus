b14829e5388a4f72c56cfa72b25f73c8843e5028
Robbie Gill <rgill@alpinenow.com>
2014-01-31 12:22:12 -0800

    [#64082294] Include api doc for license end-point

---
+++ spec/api_docs/license_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+resource License do
+  let(:user) { users(:admin) }
+
+  get '/license' do
+    before do
+      log_in user
+    end
+
+    example_request 'Get license info' do
+      status.should == 200
+    end
+  end
+end
