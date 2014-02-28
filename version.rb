8d1520d90f4eda8babb166637c4752204a3c55b3
Robbie Gill <rgill@alpinenow.com>
2014-02-28 11:06:17 -0800

    [Finishes #66685338] Bump to 3.0.7.0

--- version.rb
+++ version.rb
@@ -2,7 +2,7 @@ module Chorus
   module VERSION #:nodoc:
     MAJOR         = 3
     MINOR         = 0
-    SERVICE_PACK  = 6
+    SERVICE_PACK  = 7
     PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
