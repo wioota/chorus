d82541d4bc60f2daaddd23e72da6c10ae81f57e4
Robbie Gill <rgill@alpinenow.com>
2014-03-20 22:10:57 -0700

    Licensing already covered by the application license.

--- app/assets/javascripts/utilities/backbone_extensions.js
+++ app/assets/javascripts/utilities/backbone_extensions.js
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2011 EMC Corporation All Rights Reserved
- *
- * This software is protected, without limitation, by copyright law
- * and international treaties. Use of this software and the intellectual
- * property contained therein is expressly limited to the terms and
- * conditions of the License Agreement under which it is provided by
- * or on behalf of EMC.
- */
 var methodMap = {
     'create': 'POST',
     'update': 'PUT',
