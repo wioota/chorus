5514557920d29a4b602f8de6bc4803c440c32f10
Robbie Gill <rgill@alpinenow.com>
2014-04-14 14:43:42 -0700

    Update load path bundler deply hack to 1.6.1

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,6 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 # Ugh. needed for deploys on staging, should not affect dev/test. JP + BL
-# $LOAD_PATH << File.expand_path('../../vendor/bundler_gem/gems/bundler-1.1.0/lib/', __FILE__)
+$LOAD_PATH << File.expand_path('../../vendor/bundler_gem/gems/bundler-1.6.1/lib/', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
