3f26e214b0205086407fe2c25463071479acf235
Robbie Gill <rgill@alpinenow.com>
2014-04-17 10:39:25 -0700

    [#69775976] Removes vendored codemirror in favor of gem
     - Gem added in following commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,6 @@
 //= require vendor/jquery.ui.widget
 //= require vendor/jquery.ui.mouse
 //= require vendor/textext
-//= require vendor/code_mirror/codemirror-3.11.js
 //= require vendor/es5-shim
 //= require vendor/d3
 //= require vendor/datepicker_lang
