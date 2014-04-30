36d3b4a44fc71874f8ec583b4cba34a387af8ade
Jason Lee <jlee@alpinenow.com>
2014-04-30 11:54:15 -0700

    [#69869674] add Vertica support for generic JDBC data source
    - checking in on behalf of John Zhao

--- app/mixins/sequel/extensions/teradata.rb
+++ app/mixins/sequel/extensions/teradata.rb
@@ -3,5 +3,8 @@ module Sequel
     DATABASE_SETUP[:teradata] = proc do |db|
       com.teradata.jdbc.TeraDriver
     end
+    DATABASE_SETUP[:vertica] = proc do |db|
+      com.vertica.jdbc.Driver
+    end
   end
 end
