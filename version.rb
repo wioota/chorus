47b2fd8c231983197d2dbc15800542c149307223
Jason Lee <jlee@alpinenow.com>
2014-04-30 15:25:09 -0700

    [Finishes #70418854] update the patch build version to 4.0.1
    - checked in wrong file, last checkin

--- version.rb
+++ version.rb
@@ -2,7 +2,7 @@ module Chorus
   module VERSION #:nodoc:
     MAJOR         = 4
     MINOR         = 0
-    SERVICE_PACK  = 0
+    SERVICE_PACK  = 1
     PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
