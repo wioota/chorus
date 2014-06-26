ff4e0f7ab556dbb2435d06e79a4124325f4c18ff
Robbie Gill <rgill@alpinenow.com>
2014-06-26 15:33:17 -0700

    [#74004234] Removes warning for workflows containing hawq data sources

--- app/assets/javascripts/models/alpine_workfile.js
+++ app/assets/javascripts/models/alpine_workfile.js
@@ -84,19 +84,12 @@ chorus.models.AlpineWorkfile = chorus.models.Workfile.include(
     },
 
     notifyWorkflowLimitedDataSource: function() {
-        function isHawq(el) {
-            return el.get('entityType') === 'gpdb_database' && el.dataSource().get('isHawq');
-        }
-
         function isReadOnlyHdfs(el) {
             return el.get('entityType') === 'hdfs_data_source' && el.get('hdfsVersion') === 'Apache Hadoop 1.2';
         }
 
         _.every(this.executionLocations(), function(el) {
-            if(isHawq(el)) {
-                chorus.toast("work_flows.toast.hawq");
-                return false;
-            } else if (isReadOnlyHdfs(el)) {
+            if (isReadOnlyHdfs(el)) {
                 chorus.toast("work_flows.toast.hdfs_read_only");
                 return false;
             }
