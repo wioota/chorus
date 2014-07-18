d962ffd6cbc6140e1bce9c43dda56c9d2024e108
Robbie Gill <rgill@alpinenow.com>
2014-07-18 10:41:07 -0700

    [#58276792] Enables histogram visualization (Oracle)

--- app/services/visualization/oracle_sql.rb
+++ app/services/visualization/oracle_sql.rb
@@ -32,14 +32,6 @@ module Visualization
       raise NotImplemented
     end
 
-    def histogram_min_max_sql(o)
-      raise NotImplemented
-    end
-
-    def histogram_row_sql(o)
-      raise NotImplemented
-    end
-
     def timeseries_row_sql(o)
       raise NotImplemented
     end
