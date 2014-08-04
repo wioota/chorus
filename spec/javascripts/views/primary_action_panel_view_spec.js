a453fdda2cd3fe9aa6b7faf0b86af6b377f50e85
Dan Neumann and Gavin Morgan <pair+dneumann+gmorgan@pivotallabs.com>
2014-08-04 12:13:09 -0700

    improve PrimaryActionPanel test coverage

--- spec/javascripts/views/primary_action_panel_view_spec.js
+++ spec/javascripts/views/primary_action_panel_view_spec.js
@@ -1,63 +1,27 @@
 describe("chorus.views.PrimaryActionPanel", function () {
+//    var randomDialog = chorus.dialogs[_.sample(Object.keys(chorus.dialogs))];
+
+    var actions = [
+        {name: 'download', target: chorus.dialogs.RunAndDownload},
+        {name: 'display', target: 'destination'},
+        {name: 'explore', target: 'destination'}
+    ];
+
     beforeEach(function () {
         this.workspace = backboneFixtures.workspace();
         this.workspace.loaded = false;
-        this.view = new chorus.views.PrimaryActionPanel({model: this.workspace});
+        var constructionParams = {pageModel: this.workspace, actions: actions};
+        this.view = new chorus.views.PrimaryActionPanel(constructionParams);
     });
 
-    context("before the workspace is fetched", function () {
+    describe("a page with primary actions", function () {
         beforeEach(function () {
-            this.view.render();
+            this.page = new chorus.pages.Base();
+            this.page.primaryActionPanel = this.view;
+            this.page.render();
         });
 
-        it("does not render any actions", function () {
-            expect(this.view.$(".action").length).toBe(0);
-        });
+        itBehavesLike.aPageWithPrimaryActions(actions);
     });
 
-    context("after the workspace is fetched", function () {
-        context("and the user can update the workspace", function () {
-
-            beforeEach(function () {
-                this.modalSpy = stubModals();
-                spyOn(this.workspace, 'canUpdate').andReturn(true);
-                this.server.completeFetchFor(this.workspace);
-                this.actions = ['goold_old', 'arbitrary', 'actions'];
-            });
-
-            _.each(this.actions, function (action) {
-                it("contains an " + action + " link", function () {
-                    expect(this.view.$("." + action)).toExist();
-                });
-
-                it(action + " has the correct translation", function () {
-                    expect(this.view.$("." + action)).toContainTranslation("actions." + action);
-                });
-            }, this);
-
-
-        });
-
-        context("when the user can't update the workspace", function () {
-            beforeEach(function () {
-                spyOn(this.view.model, "canUpdate").andReturn(false);
-                this.view.render();
-            });
-
-            it("does not render any buttons", function () {
-                expect(this.view.$(".action").length).toBe(0);
-            });
-        });
-
-        context("when the workspace is archived", function () {
-            beforeEach(function () {
-                this.view.model.set("archivedAt", true);
-                this.view.render();
-            });
-
-            it("does not render any buttons", function () {
-                expect(this.view.$(".action").length).toBe(0);
-            });
-        });
-    });
 });
