8b2eeb1188d3eddddc70f1fae3a579d52aeb4f2b
Dan Neumann and Gavin Morgan <pair+dneumann+gmorgan@pivotallabs.com>
2014-08-06 13:53:28 -0700

    Prettify multiple_selection_sidebar_menu.js

--- app/assets/javascripts/views/multiple_selection_sidebar_menu.js
+++ app/assets/javascripts/views/multiple_selection_sidebar_menu.js
@@ -1,66 +1,66 @@
 chorus.views.MultipleSelectionSidebarMenu = chorus.views.Base.include(
     chorus.Mixins.ActionPanel
 ).extend({
-    constructorName: "MultiSelectionSidebarMenu",
-    templateName: "multiple_selection_sidebar_menu",
+        constructorName: "MultiSelectionSidebarMenu",
+        templateName: "multiple_selection_sidebar_menu",
 
-    setup: function(options) {
-        this.selectEvent    = options.selectEvent;
-        this.selectedModels = new chorus.collections.Base();
-        this.subscribePageEvent(this.selectEvent, this.modelSelected);
-    },
+        setup: function (options) {
+            this.selectEvent = options.selectEvent;
+            this.selectedModels = new chorus.collections.Base();
+            this.subscribePageEvent(this.selectEvent, this.modelSelected);
+        },
 
-    render: function() {
-        this._super("render", arguments);
-    },
+        render: function () {
+            this._super("render", arguments);
+        },
 
-    repopulateActions: function (selectedModels) {
-        var providerIsAFunction = (this.options.actionProvider instanceof Function);
-        this.actions = providerIsAFunction ? this.options.actionProvider(selectedModels) : this.options.actionProvider;
+        repopulateActions: function (selectedModels) {
+            var providerIsAFunction = (this.options.actionProvider instanceof Function);
+            this.actions = providerIsAFunction ? this.options.actionProvider(selectedModels) : this.options.actionProvider;
 
-        var events = this.eventBindings(this.actions, { "click .deselect_all": 'deselectAll' });
-        this.delegateEvents(events);
-        this.render();
-    },
+            var events = this.eventBindings(this.actions, { "click .deselect_all": 'deselectAll' });
+            this.delegateEvents(events);
+            this.render();
+        },
 
-    modelSelected: function(selectedModels) {
-        this.selectedModels = selectedModels;
-        this.repopulateActions(this.selectedModels);
-    },
+        modelSelected: function (selectedModels) {
+            this.selectedModels = selectedModels;
+            this.repopulateActions(this.selectedModels);
+        },
 
-    showOrHideMultipleSelectionSection: function() {
-        if(this.selectedModels.length > 1) {
-            this.$('.actions').removeClass('hidden');
-            $('#sidebar').find('.primary .actions').addClass('hidden');
-            this.$('.multiple_selection_sidebar_menu').removeClass('hidden');
-        } else if (this.selectedModels.length === 1) {
-            this.$('.actions').addClass("hidden");
-            this.$('.multiple_selection_sidebar_menu').removeClass('hidden');
-            $('#sidebar').find('.primary .actions').removeClass('hidden');
-        } else {
-            this.$('.multiple_selection_sidebar_menu').addClass('hidden');
-            $('#sidebar').find('.primary .actions').addClass('hidden');
+        showOrHideMultipleSelectionSection: function () {
+            if (this.selectedModels.length > 1) {
+                this.$('.actions').removeClass('hidden');
+                $('#sidebar').find('.primary .actions').addClass('hidden');
+                this.$('.multiple_selection_sidebar_menu').removeClass('hidden');
+            } else if (this.selectedModels.length === 1) {
+                this.$('.actions').addClass("hidden");
+                this.$('.multiple_selection_sidebar_menu').removeClass('hidden');
+                $('#sidebar').find('.primary .actions').removeClass('hidden');
+            } else {
+                this.$('.multiple_selection_sidebar_menu').addClass('hidden');
+                $('#sidebar').find('.primary .actions').addClass('hidden');
+            }
+        },
+
+        deselectAll: function (e) {
+            e.preventDefault();
+            chorus.PageEvents.trigger("selectNone");
+        },
+
+        additionalContext: function () {
+            var actions = _.map(this.actions, this.templateValues);
+            return {
+                selectedModels: this.selectedModels,
+                modelCount: this.selectedModels.length,
+                actions: actions
+            };
+        },
+
+        postRender: function () {
+            this.showOrHideMultipleSelectionSection();
+            this._super("postRender");
         }
-    },
-
-    deselectAll: function(e) {
-        e.preventDefault();
-        chorus.PageEvents.trigger("selectNone");
-    },
-
-    additionalContext: function() {
-        var actions = _.map(this.actions, this.templateValues);
-        return {
-            selectedModels: this.selectedModels,
-            modelCount: this.selectedModels.length,
-            actions: actions
-        };
-    },
-
-    postRender: function() {
-        this.showOrHideMultipleSelectionSection();
-        this._super("postRender");
-    }
 
 
-});
+    });
