5638c15c73a283808f51234da7577b84e5a65939
michel milano <michelmilano@Kailash.local>
2014-09-17 10:41:36 -0700

    [#77737524]
    updating tests for revisions to application login screen

--- spec/javascripts/views/login_view_spec.js
+++ spec/javascripts/views/login_view_spec.js
@@ -14,37 +14,9 @@ describe("chorus.views.Login", function() {
         expect(this.view.$("input[type=password]")).toHaveAttr("autocomplete", "off");
     });
 
-    it("requests the version string from the server", function() {
-        expect(this.server.requests[1].url).toHaveUrlPath("/VERSION");
-    });
-
-    it("uses a cache buster when requesting the version string", function() {
-        expect(this.server.requests[1].url.match(/buster=/)).toBeTruthy();
-    });
-
     it("focuses the username field by default", function() {
         expect($.fn.focus).toHaveBeenCalled();
         expect($.fn.focus.lastCall().object).toBe("input[name='username']");
-    });
-
-    describe("when the version string is returned", function() {
-        beforeEach(function() {
-            this.version = "0123456789ABCDEF012";
-            this.server.requests[1].respond(200, {}, this.version);
-        });
-
-        it("inserts the version string", function() {
-            expect(this.view.$(".legal .version")).toHaveText(this.version);
-        });
-
-        it("limits the version to 19 characters", function() {
-            this.server.requests[1].respond(200, {}, "0123456789ABCDEF012345");
-            expect(this.view.$(".legal .version")).toHaveText("0123456789ABCDEF012");
-        });
-
-        it("sets the version title to the full version", function() {
-            expect(this.view.$(".legal .version").attr('title')).toBe("Version " + this.version);
-        });
     });
 
     describe("when the status is returned", function() {
@@ -121,7 +93,6 @@ describe("chorus.views.Login", function() {
             context("from the same user that timed out", function() {
                 beforeEach(function() {
                     chorus.session.user().set('id', '2');
-
                     this.view.model.trigger('saved', this.view.model);
                 });
 
@@ -133,7 +104,6 @@ describe("chorus.views.Login", function() {
             context("from a different user that timed out", function() {
                 beforeEach(function() {
                     chorus.session.user().set('id', '3');
-
                     this.view.model.trigger('saved', this.view.model);
                 });
 
@@ -142,6 +112,5 @@ describe("chorus.views.Login", function() {
                 });
             });
         });
-
     });
 });
