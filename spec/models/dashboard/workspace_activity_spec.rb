327e3e53efad4c506ffa5c2d9974627f5a1e07cc
Michael Souza <msouza@alpinenow.com>
2014-10-03 17:38:19 -0700

    [Finishes #79638724, #79639062] Workspace activity with hovercard and date selection

--- spec/models/dashboard/workspace_activity_spec.rb
+++ spec/models/dashboard/workspace_activity_spec.rb
@@ -2,18 +2,39 @@ require 'spec_helper'
 
 describe Dashboard::WorkspaceActivity do
   let(:user) { users(:the_collaborator) }
-  let(:model) { described_class.new({:user => user}).fetch! }
 
-  describe '#result' do
-    let(:result) { model.result }
+  describe '#result by date groupings' do
+    test_cases = [
+      { :additional => {} },
+      { :additional => { :date_group => 'day' } },
+      { :additional => { :date_group => 'week', :date_parts => 4} },
+      { :additional => { :date_group => 'day',  :date_parts => 7 } },
+      { :additional => { :date_group => 'week', :date_parts => 12} }
+    ]
 
-    it 'has the correct keys' do
-      elem = result.first
-      puts elem.to_yaml
-      elem.should have_key(:event_count)
-      elem.should have_key(:workspace_id)
-      elem.should have_key(:date_part)
-      elem.should have_key(:rank)
+    test_cases.each_with_index do |tc, i|
+      it "has the correct structure for tc #{tc}" do
+        @model = described_class.new({ :user => user }.merge(tc)).fetch!
+        @result = @model.result
+
+        @result.should have_key(:workspaces)
+        ws = @result[:workspaces].first
+        ws.should have_key(:workspace_id)
+        ws.should have_key(:name)
+        ws.should have_key(:summary)
+        ws.should have_key(:event_count)
+
+        @result.should have_key(:events)
+        ev = @result[:events].first
+        ev.should have_key(:event_count)
+        ev.should have_key(:workspace_id)
+        ev.should have_key(:date_part)
+        ev.should have_key(:rank)
+
+        # Expect there to be (num workspaces)*(num date parts + 1) events
+        r1 = @result[:events].length % @result[:workspaces].length
+        r1.should eq(0)
+      end
     end
   end
 end
