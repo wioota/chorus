c7356f610e55069d4fdbad46186ca69ef9d9a290
Jason Lee <jlee@alpinedatalabs.com>
2014-10-14 15:36:14 -0700

    [Finishes #80390790] -configure option for individual modules is now preserved when changing dashboard setup

--- app/models/dashboard_item.rb
+++ app/models/dashboard_item.rb
@@ -3,7 +3,7 @@ class DashboardItem < ActiveRecord::Base
   ALLOWED_MODULES = %w(SiteSnapshot WorkspaceActivity ActivityStream ProjectCardList RecentWorkfiles)
   DEFAULT_MODULES = ALLOWED_MODULES.first(3)
 
-  attr_accessible :name, :location
+  attr_accessible :name, :location, :options
 
   validates_inclusion_of :name, :in => ALLOWED_MODULES
 end
