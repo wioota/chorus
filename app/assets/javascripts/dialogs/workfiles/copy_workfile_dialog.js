45ffbc7a8523f3e0cd5f74bf7ff15f011615f5b6
Michel Milano <mmilano+git@gmail.com>
2014-11-12 12:06:57 -0800

    main changes: revise success message to display link to the copied file - allows user to click the link to go directly to the newly copied file.

--- app/assets/javascripts/dialogs/workfiles/copy_workfile_dialog.js
+++ app/assets/javascripts/dialogs/workfiles/copy_workfile_dialog.js
@@ -12,7 +12,6 @@ chorus.dialogs.CopyWorkfile = chorus.dialogs.PickWorkspace.extend({
         this.requiredResources.add(this.workfile);
         this.requiredResources.add(this.collection);
         this.workfile.fetch();
-
         this._super("setup", arguments);
     },
 
@@ -23,15 +22,14 @@ chorus.dialogs.CopyWorkfile = chorus.dialogs.PickWorkspace.extend({
     submit: function() {
         var self = this;
         var workfile = this.workfile;
-
         var params = {
             workspace_id: this.selectedItem().get("id")
         };
-
         var description = workfile.get("description");
         if (description) {
             params.description = description;
         }
+
         $.ajax({
             url: "/workfiles/" + this.workfile.get("id") + "/copy",
             type: "POST",
@@ -40,7 +38,13 @@ chorus.dialogs.CopyWorkfile = chorus.dialogs.PickWorkspace.extend({
             success: function(data) {
                 self.closeModal(true);
                 var copiedWorkfile = new chorus.models.Workfile(workfile.parse(data));
-                chorus.toast("workfile.copy_dialog.toast", {workfileTitle: copiedWorkfile.get("fileName"), workspaceNameTarget: self.selectedItem().get("name")});
+
+                // construct message to user
+                // File Name = copiedWorkfile.get("fileName")
+                var copiedFileUrl = copiedWorkfile.showUrl();
+                var copiedFileLink_html = "<a href='" + copiedFileUrl + "'>" + copiedWorkfile.get("fileName") + "</a>";
+
+                chorus.toast("workfile.copy_dialog.toast", {workfileURL: copiedFileLink_html, workspaceNameTarget: self.selectedItem().get("name")});
             },
 
             error: function(xhr) {
