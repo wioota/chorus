4dc39bef5d8c2bacb2bde848f968d9619bae8d51
chesterxgchen <chester@alpinenow.com>
2015-02-11 11:49:40 -0800

    update version to 5.2.1 for release branch 5.2, reviewed by John Z

--- version.rb
+++ version.rb
@@ -2,7 +2,7 @@ module Chorus
   module VERSION #:nodoc:
     MAJOR         = 5
     MINOR         = 2
-    SERVICE_PACK  = 0
+    SERVICE_PACK  = 1
     PATCH         = 0
 
     STRING = [MAJOR, MINOR, SERVICE_PACK, PATCH, ENV['BUILD_NUMBER']].compact.join('.')
