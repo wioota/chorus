b7b76c51289d57506287761692669a2e5915edce
Michel Milano <mmilano+git@gmail.com>
2015-02-12 18:49:30 -0800

    revisions to make a new error page in the app

--- spec/javascripts/pages/error_page_spec.js
+++ spec/javascripts/pages/error_page_spec.js
@@ -11,7 +11,7 @@ describe("chorus.pages.ErrorPage", function() {
     });
 
     it("has the translations for the title", function() {
-        expect(this.page.$('.title')).toContainText(this.page.pageOptions.title);
+        expect(this.page.$('.heading')).toContainText(this.page.pageOptions.title);
     });
 
     it("has the translations for the textbox content", function() {
@@ -22,12 +22,12 @@ describe("chorus.pages.ErrorPage", function() {
         expect(this.page.$('.content')).toContainText(this.page.pageOptions.message);
     });
 
-    it("has the translations for the button", function() {
-        expect(this.page.$('button.submit')).toContainTranslation("actions.home");
+    it("has the translations for the link to home", function() {
+        expect(this.page.$('.link_home')).toContainTranslation("application.message.choicenext.link.home");
     });
 
-    it("navigates to the homepage on clicking the button", function() {
-        this.page.$('button.submit').click();
+    it("navigates to the homepage by clicking the link to home", function() {
+        this.page.$('.link_home').click();
         expect(chorus.router.navigate).toHaveBeenCalledWith("#");
     });
 });
