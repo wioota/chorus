cfedd9bf2e4d22aaf17bb6d899d384018ce8b4ed
Michel Milano <mmilano@alpinenow.com>
2015-02-23 19:29:57 -0800

    message revision +  property path update (namespace)

--- spec/javascripts/views/dataset/dataset_create_chorus_view_sidebar_spec.js
+++ spec/javascripts/views/dataset/dataset_create_chorus_view_sidebar_spec.js
@@ -35,7 +35,7 @@ describe("chorus.views.CreateChorusViewSidebar", function() {
             });
 
             it("has the right text", function() {
-                expect(addJoinLink.text()).toMatchTranslation("dataset.chorusview.sidebar.add_join");
+                expect(addJoinLink.text()).toMatchTranslation("dataset.chorusview.sidebar.add_join.action");
             });
 
             it("has a reference to a chorus view derived from the current dataset", function() {
