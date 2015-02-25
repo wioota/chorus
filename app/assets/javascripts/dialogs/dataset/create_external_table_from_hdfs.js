7393d4b46550ccc44da647729b8c5eab9907d44e
Michel Milano <mmilano@alpinenow.com>
2015-02-24 16:20:42 -0800

    improvements to the toast messages - create external table - edit data source settings

--- app/assets/javascripts/dialogs/dataset/create_external_table_from_hdfs.js
+++ app/assets/javascripts/dialogs/dataset/create_external_table_from_hdfs.js
@@ -45,7 +45,7 @@ chorus.dialogs.CreateExternalTableFromHdfs = chorus.dialogs.NewTableImportCSV.ex
 
     saved: function() {
         this.closeModal();
-        chorus.toast("hdfs.create_external.success", {workspaceName: this.workspaceName, tableName: this.model.get("tableName")});
+        chorus.toast("hdfs.create_external.success.message", {workspaceName: this.workspaceName, tableName: this.model.get("tableName")});
         chorus.PageEvents.trigger("csv_import:started");
     },
 
