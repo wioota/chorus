74f3a4fd62b855efcb95ac568b68400dcd98ff6d
Michel Milano <mmilano@alpinenow.com>
2015-02-25 11:05:25 -0800

    some notes for making the empty message

--- app/assets/javascripts/views/dataset/dataset_list_view.js
+++ app/assets/javascripts/views/dataset/dataset_list_view.js
@@ -21,12 +21,28 @@ chorus.views.DatasetList = chorus.views.PageItemList.extend({
             var linkText = Handlebars.helpers.linkTo("#/data_sources", t("dataset.browse.linkText"));
             var noDatasetEl = $("<div class='browse_more'></div>");
 
+// TODO: add fuincationality so that the empty data message displays using the app standard empty box
+//             var emptyStateEl = $("<div class="empty_state"></div>");
+//             var emptyStateTitle = $("<div class="empty_message"></div>");
+//             var emptyStateTip = $("<div class="text_tip"></div>");
+            
+//                    <div class="empty_message">
+//                       {{t "dashboard.recent_workfiles.empty.message"}}
+//                     </div>
+//                     <div class="text_tip">
+//                         {{t "dashboard.recent_workfiles.empty.tip"}}
+//                     </div>
+
+
             var hintText;
             if (this.collection.hasFilter && this.collection.hasFilter()) {
+                // empty filtered list
                 hintText = t("dataset.filtered_empty");
             } else if (this.collection.attributes.workspaceId) {
+                // empty workspace
                 hintText = t("dataset.browse_more_workspace", {linkText: linkText});
             } else {
+                // empty datasets in schema?
                 hintText = t("dataset.browse_more_data_source", {linkText: linkText});
             }
 
