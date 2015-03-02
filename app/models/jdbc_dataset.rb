f8e19d659bb649d59b7303698331ba08f9c269b5
Michael Souza <mike@michaelsouza.com>
2015-03-02 14:48:25 -0800

    DEV-6160 Hive2 basic preview, and some infrastructure to consolidate the miscellaneous overrides for JDBC data source display.

--- app/models/jdbc_dataset.rb
+++ app/models/jdbc_dataset.rb
@@ -5,7 +5,23 @@ class JdbcDataset < RelationalDataset
     'JdbcDatasetColumn'
   end
 
+  def inject_overrides
+    if @om.nil?
+      @om = JdbcOverrides::overrides_by_db_url(data_source.url)
+      self.extend(@om::DatasetOverrides) if !@om::DatasetOverrides.nil?
+      return true
+    end
+
+    false
+  end
+
+  def after_initialize
+    inject_overrides
+  end
+
   def all_rows_sql(limit = nil)
+    return all_rows_sql(limit) if inject_overrides
+
     Arel::Table.new(%("#{schema_name}"."#{name}")).project('*').to_sql
   end
 end
