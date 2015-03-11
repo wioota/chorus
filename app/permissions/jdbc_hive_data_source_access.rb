65ac3a0d9e564334f3ed79e1ae0c1acb1ad7bab6
Jason Lee <jlee@alpinedatalabs.com>
2015-03-11 14:31:58 -0700

    hive kerberos stuff

---
+++ app/permissions/jdbc_hive_data_source_access.rb
@@ -0,0 +1,19 @@
+class JdbcHiveDataSourceAccess < DefaultAccess
+  def self.data_sources_for(current_user)
+    JdbcHiveDataSourceAccess.accessible_to(current_user)
+  end
+
+  def show?(data_source)
+    true
+  end
+
+  def edit?(data_source)
+    data_source.owner == current_user || current_user.admin?
+  end
+
+  def show_contents?(data_source)
+    #data_source.shared? || current_user.data_source_accounts.exists?(:data_source_id => data_source.id)
+    true
+  end
+end
+
