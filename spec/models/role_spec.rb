79cc8b3359f992f86db1279d05c1faf23bc0b86b
andrewlngdn <andrew@alpinenow.com>
2015-03-17 16:00:31 -0700

    removed commented code

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,15 +5,5 @@ describe Role do
     it { should have_many(:permissions) }
     it { should have_and_belong_to_many(:users) }
     it { should have_and_belong_to_many(:groups) }
-
-   # it "has_many permissions" do
-   #   Role.reflect_on_association(:permissions).macro.should == :has_many
-   # end
-   # it "has_and_belong_to_many users" do
-   #   Role.reflect_on_association(:users).macro.should == :has_and_belongs_to_many
-   # end
-   # it "has_and_belong_to_many groups" do
-   #   Role.reflect_on_association(:groups).macro.should == :has_and_belongs_to_many
-   # end
   end
 end
