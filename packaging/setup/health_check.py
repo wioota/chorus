bbe4e7efb6304a11e00daf0dae3161e215e7abd7
Hao Zou <hao@alpinenow.com>
2015-03-19 15:54:33 -0700

    health_check

--- packaging/setup/health_check.py
+++ packaging/setup/health_check.py
@@ -1,2 +1,8 @@
-def health_check():
-    pass
+import subprocess
+def health_check(args=''):
+    command = "%s %s" % (os.path.join(os.path.abspath(__file__), "atk"), args)
+    p = subprocess.Popen("", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    print  stdout
+    print stderr
+    return p.returncode, stdout, stderr
