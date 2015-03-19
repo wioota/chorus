f3870e0b27d923754bcbb14b160ee640d8a9e9a7
Hao Zou <hao@alpinenow.com>
2015-03-19 13:28:20 -0700

    minor change

--- packaging/setup/chorus_executor.py
+++ packaging/setup/chorus_executor.py
@@ -23,13 +23,13 @@ class ChorusExecutor:
             postgres_bin_path = self.release_path
         command = "PATH=%s/postgres/bin:$PATH && %s" % (postgres_bin_path, command)
         logger.debug(command)
-        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
         if stdout:
             logger.debug(stdout)
         if stderr:
             logger.debug(stderr)
-        return stdout, stderr
+        return p.returncode, stdout, stderr
 
     def extract_postgres(self, package_name):
         self.run("tar xzfv %s -C %s" % (os.path.join(self.release_path, "packaging/postgres/" + package_name),\
@@ -38,13 +38,19 @@ class ChorusExecutor:
     def chorus_control(self, command):
        command = "CHORUS_HOME=%s %s %s/packaging/chorus_control.sh %s" % \
                (self.release_path, self.alpine_env(), self.release_path, command)
-       return self.run(command)
+       (ret, stdout, stderr) = self.run(command)
+       if ret != 0:
+           raise Exception("chorus_constrol.sh command failed")
+       return (ret, stdout, stderr)
 
     def previous_chorus_control(self, command):
         command = "CHORUS_HOME=%s %s %s %s" % \
                 (os.path.join(self.chorus_path, "current"), self.alpine_env(),\
                 os.path.join(self.chorus_path, "chorus_control.sh"), command)
-        self.run(command, os.path.join(self.chorus_path, "current"))
+        (ret, stdout, stderr) = self.run(command)
+       if ret != 0:
+           raise Exception("chorus_constrol.sh command failed")
+       return (ret, stdout, stderr)
 
     def alpine_env(self):
         return "ALPINE_HOME=%s/alpine-current ALPINE_DATA_REPOSITORY=%s/shared/ALPINE_DATA_REPOSITORY" % \
@@ -58,27 +64,27 @@ class ChorusExecutor:
         #self.run("killall chorus")
     def start_postgres(self):
         logger.info("Starting postgres...")
-        stdout, stderr = self.chorus_control("start postgres")
+        ret, stdout, stderr = self.chorus_control("start postgres")
         if "postgres failed" in stdout:
             raise PSQLException(stdout)
 
     def stop_postgres(self):
         logger.info("Stopping postgres")
-        stdout, stderr = self.chorus_control("stop postgres")
+        ret, stdout, stderr = self.chorus_control("stop postgres")
         if "postgres failed" in stdout:
             raise PSQLException(stdout)
 
     def initdb(self, data_path, database_user):
         command = "initdb --locale=en_US.UTF-8 -D %s/db --auth=md5 --pwfile=%s/postgres/pwfile --username=%s" % \
                 (data_path, self.release_path, database_user)
-        stdout, stderr = self.run(command)
+        ret, stdout, stderr = self.run(command)
         if "exists but is not empty" in stderr:
             logger.warning(stderr)
 
     def rake(self, command):
         command = "cd %s && RAILS_ENV=production bin/ruby -S bin/rake %s --trace" % \
                 (self.release_path, command)
-        stdout, stderr = self.run(command)
+        ret, stdout, stderr = self.run(command)
         if "rake aborted" in stderr:
             raise RAKEException(stderr)
 
