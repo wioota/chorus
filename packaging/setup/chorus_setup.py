3fad61a693363107a9c25267b76f1d771eb9f5df
Hao Zou <hao@alpinenow.com>
2015-03-19 09:39:41 -0700

    minor change

--- packaging/setup/chorus_setup.py
+++ packaging/setup/chorus_setup.py
@@ -87,8 +87,8 @@ class ChorusSetup:
 
     def construct_data_structure(self):
         logger.info("construct data structure in %s" % options.data_path)
-        for folder in ["db","system","log","solr"]:
-            self._mkdir_p(folder)
+        for folder in ["db","system","log","solr/data"]:
+            executor.run("mkdir -p %s" % folder)
 
     def link_shared_config(self):
         logger.info("Linking shared configuration files to %s/config" % self.release_path)
