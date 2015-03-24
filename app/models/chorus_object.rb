75be02390a98afc896b2ad558c775c39f1de783d
andrewlngdn <andrew@alpinenow.com>
2015-03-24 16:46:39 -0700

    added missing associaion to chorus_object

--- app/models/chorus_object.rb
+++ app/models/chorus_object.rb
@@ -1,5 +1,6 @@
 class ChorusObject < ActiveRecord::Base
   attr_accessible :class_id, :instance_id, :parent_class_id, :parent_class_name, :permission_mask
 
+  belongs_to :chorus_class
   belongs_to :scope
 end
