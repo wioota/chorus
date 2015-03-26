3e5c237f43f12043097317f7d702a5519504b0b6
andrewlngdn <andrew@alpinenow.com>
2015-03-25 18:10:00 -0700

    updated migration for change permissions

--- db/migrate/20150325182454_change_permission_class_id_to_chorus_class_id.rb
+++ db/migrate/20150325182454_change_permission_class_id_to_chorus_class_id.rb
@@ -1,5 +1,11 @@
 class ChangePermissionClassIdToChorusClassId < ActiveRecord::Migration
-  def change
+  def up
     rename_column :permissions, :class_id, :chorus_class_id
+    change_column :permissions, :chorus_class_id, 'integer USING CAST("chorus_class_id" AS integer)'
+  end
+
+  def down
+    change_column :permissions, :chorus_class_id, :string
+    rename_column :permissions, :chorus_class_id, :class_id
   end
 end
