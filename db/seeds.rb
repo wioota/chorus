0ba58201b9b0ae35fb046b3e3465b5a79149fd20
andrewlngdn <andrew@alpinenow.com>
2015-03-25 18:10:37 -0700

    added seeds for RGP. FactoryGirl.create(:user) is acting weird and creating 2 users so I am moving away from that

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+FACTORY_GIRL_SEQUENCE_OFFSET = 44444
 require 'user'
+require 'factory_girl'
+require 'spec/factories/users.rb'
+require 'spec/factories/roles_groups_permissions.rb'
+require 'spec/factories/factories.rb'
 
 ActiveRecord::Base.connection.schema_cache.clear!
 
@@ -34,64 +39,81 @@ unless User.where(:username => "chorusadmin").present?
   user.admin = true
   user.save!
 end
+chorusadmin = User.find_by_username("chorusadmin")
 
+dev_workspace1 = FactoryGirl.create(:workspace, :name => "Development workspace one")
+mark_workspace1 = FactoryGirl.create(:workspace, :name => "Marketing workspace one")
+
+data_source_info = {
+    "name"=>"PG DB",
+     "description"=>"",
+     "host"=>"10.0.0.168",
+     "port"=>"5432",
+     "db_name"=>"postgres",
+     "db_username"=>"miner_demo",
+     "db_password"=>"miner_demo",
+     "ssl"=>false,
+     "shared"=>false,
+     "high_availability"=>false,
+     "hive_kerberos"=>false
+}
+
+pg_data_source= DataSource.create_for_entity_type("pg_data_source", chorusadmin, data_source_info)
+pg_data_source= DataSource.create_for_entity_type("pg_data_source", chorusadmin, data_source_info.merge({"name" => "Other PG DB"}))
+chorus_objects = [dev_workspace1, pg_data_source, mark_workspace1]
+
+# --- ROLES GROUPS PERMISSIONS ---
+
+# --- administrator users and role ---
+admin = FactoryGirl.create(:user, :username => "admin")
+admin_role = FactoryGirl.create(:role, :name => "Admin")
+admin_role.users << [admin]
+
+# --- developer users and role ---
+#dev1 = FactoryGirl.create(:user, :username => "dev1")
 dev1 = User.create(
-    :username => "developer1",
-    :first_name => "Dev",
+    :username => "dev1",
+    :first_name => "dev",
     :last_name => "1",
     :email => "dev1@example.com",
     :password => "secret",
-    :password_confirmation => "secret"
+    :password_confrmation => "secret"
 )
+dev2 = FactoryGirl.create(:user, :username => "dev2")
+dev_role = FactoryGirl.create(:role, :name => "Developer")
+dev_role.users << [dev1, dev2]
 
-dev2 = User.create(
-    :username => "developer2",
-    :firstname => "Dev",
-    :lastname => "2",
-    :email => "dev2@example.com",
-    :password => "secret",
-    :password_confirmation => "secret"
-)
+# --- collaborator users and role ---
+collab1 = FactoryGirl.create(:user, :username => "collab1")
+collab2 = FactoryGirl.create(:user, :username => "collab2")
+collab_role = FactoryGirl.create(:role, :name => "Collaborator", :description => "Role for collaborator")
+collab_role.users << [collab1, collab2]
 
-dev3 = User.create(
-    :username => "developer3",
-    :firstname => "Dev",
-    :lastname => "3",
-    :email => "dev3@example.com",
-    :password => "secret",
-    :password_confirmation => "secret"
-)
+# --- misc group and role ---
+#guser = FactoryGirl.create(:user, :username => "grouped_user")
+#misc_group = FactoryGirl.create(:group, :name => "Generic Group", :description => "A group of different users with different roles")
+#misc_group.users << [dev1, mark1, guser]
+#misc_role = FactoryGirl.create(:role, :name => "Misc Role", :description => "This role is attached to a group")
+#misc_group.roles << misc_role
 
-data1 = User.create(
-    :username => "datascientist1",
-    :first_name => "Data",
-    :last_name => "Scientist",
-    :email => "data1@example.com",
-    :password => "secret",
-    :password_confirmation => "secret"
-)
+# --- objects and classes for developer role ---
+chorus_objects.each do |obj|
+  chorus_class = ChorusClass.find_or_create_by_name(obj.class.name)
+  chorus_object = chorus_class.chorus_objects.find_or_create_by_instance_id(obj.id)
+  puts "Adding #{chorus_object} to #{chorus_class}"
+end
 
-data2 = User.create(
-    :username => "datascientist2",
-    :first_name => "Data",
-    :last_name => "Scientist",
-    :email => "data2@example.com",
-    :password => "secret",
-    :password_confirmation => "secret"
-)
-
-
-# --- ROLES---
-
-dev_role = Role.create(
-    :name => "Developer",
-    :descroption => "Developer role"
-)
-dev_role.users << [dev1, dev2, dev3]
-
-data_scientist_role = Role.create(
-    :name => "Data Scientist",
-    :description => "Data scientist role"
-)
-data_scientist_role.users << [data1, data2]
+# --- permissions ---
+cannot_view_workspace = Permission.new
+cannot_view_workspace.permissions_mask = 0
+read_only_workspace = Permission.new
+read_only_workspace.permissions_mask = 1
+edit_workspace = Permission.new
+edit_workspace.permissions_mask = 2
+#admin_role.permissions << edit_workspace
+edit_workspace.role = admin_role
+edit_workspace.role = dev_role
+read_only_workspace.role = collab_role
+#collab_role.permissions << read_only_workspace
+ChorusClass.find_by_name("Workspace").permissions << [read_only_workspace, edit_workspace]
 
