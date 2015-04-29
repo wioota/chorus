a4b756983691e9140e03cea0ed636b2278c73d58
andrewlngdn <andrew@alpinenow.com>
2015-04-29 12:06:32 -0700

    remove Allowy from userImagesController

--- app/controllers/user_images_controller.rb
+++ app/controllers/user_images_controller.rb
@@ -1,12 +1,15 @@
 class UserImagesController < ImagesController
+  before_filter :require_referenced_user, :only => :create
   protected
 
   def load_entity
     @entity = User.find(params[:user_id])
+    @user = @entity
   end
 
   def authorize_create!
-    authorize! :update, @entity
+    # Remove this method when 'authorize_create!' is taken out of
+    # ImagesController
   end
 
   def authorize_show!
